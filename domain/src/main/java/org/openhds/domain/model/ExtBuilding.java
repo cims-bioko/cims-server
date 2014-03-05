a7c1188679c7e8e3b35f03e6970d383e0accbfbb
Ben Heasly <benjamin.heasly@gmail.com>
2014-03-05 15:12:18 -0500

    Add ExtBuilding to Hibernate config.

--- domain/src/main/java/org/openhds/domain/model/ExtBuilding.java
+++ domain/src/main/java/org/openhds/domain/model/ExtBuilding.java
@@ -1,12 +1,9 @@
 package org.openhds.domain.model;
 
 import java.io.Serializable;
-import java.util.Calendar;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.annotations.Cascade;
