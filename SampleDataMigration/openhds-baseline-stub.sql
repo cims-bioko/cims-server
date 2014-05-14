72dd292ab03c644621c761b41c83191c75ed2125
Ben Heasly <benjamin.heasly@gmail.com>
2014-05-14 13:03:47 -0400

    Rename user and region descriptions for data import.

--- SampleDataMigration/openhds-baseline-stub.sql
+++ SampleDataMigration/openhds-baseline-stub.sql
@@ -1,4 +1,4 @@
-INSERT INTO `user` (uuid, firstName, lastName, fullName, description, username, password, lastLoginTime, deleted) VALUES ('e67f2870c59c11e39c1a0800200c9a66', 'Ben', 'Heasly', 'Ben Heasly Import Script', 'Bulk import of sample data from Baney district spreadsheet', 'bsh-bulk-import', 'bsh-bulk-import', unix_timestamp(), false);
+INSERT INTO `user` (uuid, firstName, lastName, fullName, description, username, password, lastLoginTime, deleted) VALUES ('e67f2870c59c11e39c1a0800200c9a66', 'Ben', 'Heasly', 'Ben Heasly Import Script', 'Bulk import from Master_DB_All_Houses - Round19v1 spreadsheet', 'bsh-bulk-import', 'bsh-bulk-import', unix_timestamp(), false);
 
 INSERT INTO `user_roles` (user_uuid, role_uuid) VALUES ('e67f2870c59c11e39c1a0800200c9a66', 'ROLE1');
 
@@ -10,4 +10,4 @@ INSERT INTO `locationhierarchylevel` (uuid,keyIdentifier,name) VALUES('e8033d21c
 INSERT INTO `locationhierarchylevel` (uuid,keyIdentifier,name) VALUES('e8033d22c5ac11e39c1a0800200c9a66',4,'MapArea');
 INSERT INTO `locationhierarchylevel` (uuid,keyIdentifier,name) VALUES('e8033d23c5ac11e39c1a0800200c9a66',5,'Sector');
 
-INSERT INTO `locationhierarchy` (uuid,name,extId,level_uuid,parent_uuid) VALUES ('36ec15f0c5ae11e39c1a0800200c9a66','Bioko Island','BI','e8031610c5ac11e39c1a0800200c9a66','HIERARCHY_ROOT');
+INSERT INTO `locationhierarchy` (uuid,name,extId,level_uuid,parent_uuid) VALUES ('36ec15f0c5ae11e39c1a0800200c9a66','Bioko','BI','e8031610c5ac11e39c1a0800200c9a66','HIERARCHY_ROOT');
