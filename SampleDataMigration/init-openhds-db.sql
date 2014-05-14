3f2b1a0afb60830eb54f594de670c1e1f1849e12
Ben Heasly <benjamin.heasly@gmail.com>
2014-05-14 10:30:41 -0400

    OpenHDS database privileges for USM subnet.

--- SampleDataMigration/init-openhds-db.sql
+++ SampleDataMigration/init-openhds-db.sql
@@ -2,5 +2,6 @@ DROP USER 'openhds'@'localhost';
 CREATE USER 'openhds'@'localhost' IDENTIFIED BY 'openhds';
 DROP DATABASE `openhds`;
 CREATE DATABASE `openhds`;
-GRANT ALL PRIVILEGES ON openhds.* TO 'openhds'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'openhds'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'openhds'@’130.111.%’;
 FLUSH PRIVILEGES;
