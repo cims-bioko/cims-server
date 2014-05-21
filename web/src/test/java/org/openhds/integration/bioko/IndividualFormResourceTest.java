d9ec6b4f07f79096b7ef8bd526c3ce787edf7d1e
Ben Heasly <benjamin.heasly@gmail.com>
2014-05-20 21:18:55 -0400

    IndividualFormResource processes forms implementation.
    Processes Bioko Individual forms to create/update Individual,
    Membership, and Residency.

--- web/src/test/java/org/openhds/integration/bioko/IndividualFormResourceTest.java
+++ web/src/test/java/org/openhds/integration/bioko/IndividualFormResourceTest.java
@@ -5,10 +5,16 @@ import static org.springframework.test.web.server.result.MockMvcResultMatchers.c
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.xpath;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openhds.dao.service.GenericDao;
+import org.openhds.domain.model.Individual;
+import org.openhds.domain.model.Membership;
+import org.openhds.domain.model.Residency;
 import org.openhds.integration.util.WebContextLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -38,6 +44,32 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 @DatabaseSetup(value = "/individualFormResourceDb.xml", type = DatabaseOperation.REFRESH)
 public class IndividualFormResourceTest {
 
+    private static final String A_DATE = "2000-01-01T00:00:00-05:00";
+    private static final String INDIVIDUAL_FORM_POST_XML = "<individualForm>"
+            + "<processedByMirth>false</processedByMirth>"
+            + "<fieldWorkerExtId>FWEK1D</fieldWorkerExtId>" + "<collectionDateTime>"
+            + A_DATE
+            + "</collectionDateTime>"
+            + "<householdExtId>householdId</householdExtId>"
+            + "<individualExtId>TWELVE_DIGIT</individualExtId>"
+            + "<individualFirstName>Test First</individualFirstName>"
+            + "<individualLastName>Test Last</individualLastName>"
+            + "<individualOtherNames>Test Other</individualOtherNames>"
+            + "<individualAge>10</individualAge>"
+            + "<individualAgeUnits>years</individualAgeUnits>"
+            + "<individualDateOfBirth>"
+            + A_DATE
+            + "</individualDateOfBirth>"
+            + "<individualGender>M</individualGender>"
+            + "<individualRelationshipToHeadOfHousehold>2</individualRelationshipToHeadOfHousehold>"
+            + "<individualPhoneNumber>12345678890</individualPhoneNumber>"
+            + "<individualOtherPhoneNumber>0987654321</individualOtherPhoneNumber>"
+            + "<individualLanguagePreference>English</individualLanguagePreference>"
+            + "<individualPointOfContactName></individualPointOfContactName>"
+            + "<individualPointOfContactPhoneNumber></individualPointOfContactPhoneNumber>"
+            + "<individualDip>12345</individualDip>"
+            + "<individualMemberStatus>permanent</individualMemberStatus>" + "</individualForm>";
+
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -61,33 +93,6 @@ public class IndividualFormResourceTest {
 
     @Test
     public void testPostIndividualFormXml() throws Exception {
-        final String A_DATE = "2000-01-01T00:00:00-05:00";
-        final String INDIVIDUAL_FORM_POST_XML = "<individualForm>"
-                + "<processedByMirth>false</processedByMirth>"
-                + "<fieldWorkerExtId>FWTEST</fieldWorkerExtId>" + "<collectionDateTime>"
-                + A_DATE
-                + "</collectionDateTime>"
-                + "<householdExtId>HOUSE_ID</householdExtId>"
-                + "<individualExtId>INDIVIDUAL_ID</individualExtId>"
-                + "<individualFirstName>Test First</individualFirstName>"
-                + "<individualLastName>Test Last</individualLastName>"
-                + "<individualOtherNames>Test Other</individualOtherNames>"
-                + "<individualAge>10</individualAge>"
-                + "<individualAgeUnits>years</individualAgeUnits>"
-                + "<individualDateOfBirth>"
-                + A_DATE
-                + "</individualDateOfBirth>"
-                + "<individualGender>Male</individualGender>"
-                + "<individualRelationshipToHeadOfHousehold>head</individualRelationshipToHeadOfHousehold>"
-                + "<individualPhoneNumber>12345678890</individualPhoneNumber>"
-                + "<individualOtherPhoneNumber>0987654321</individualOtherPhoneNumber>"
-                + "<individualLanguagePreference>English</individualLanguagePreference>"
-                + "<individualPointOfContactName></individualPointOfContactName>"
-                + "<individualPointOfContactPhoneNumber></individualPointOfContactPhoneNumber>"
-                + "<individualDip>12345</individualDip>"
-                + "<individualMemberStatus>permanent</individualMemberStatus>"
-                + "</individualForm>";
-
         mockMvc.perform(
                 post("/individualForm").session(session).accept(MediaType.APPLICATION_XML)
                         .contentType(MediaType.APPLICATION_XML)
@@ -95,20 +100,20 @@ public class IndividualFormResourceTest {
                 .andExpect(status().isCreated())
                 .andExpect(content().mimeType(MediaType.APPLICATION_XML))
                 .andExpect(xpath("/individualForm/processedByMirth").string("false"))
-                .andExpect(xpath("/individualForm/fieldWorkerExtId").string("FWTEST"))
+                .andExpect(xpath("/individualForm/fieldWorkerExtId").string("FWEK1D"))
                 .andExpect(xpath("/individualForm/collectionDateTime").string(A_DATE))
-                .andExpect(xpath("/individualForm/householdExtId").string("HOUSE_ID"))
-                .andExpect(xpath("/individualForm/individualExtId").string("INDIVIDUAL_ID"))
+                .andExpect(xpath("/individualForm/householdExtId").string("householdId"))
+                .andExpect(xpath("/individualForm/individualExtId").string("TWELVE_DIGIT"))
                 .andExpect(xpath("/individualForm/individualFirstName").string("Test First"))
                 .andExpect(xpath("/individualForm/individualLastName").string("Test Last"))
                 .andExpect(xpath("/individualForm/individualOtherNames").string("Test Other"))
                 .andExpect(xpath("/individualForm/individualAge").string("10"))
                 .andExpect(xpath("/individualForm/individualAgeUnits").string("years"))
                 .andExpect(xpath("/individualForm/individualDateOfBirth").string(A_DATE))
-                .andExpect(xpath("/individualForm/individualGender").string("Male"))
+                .andExpect(xpath("/individualForm/individualGender").string("M"))
                 .andExpect(
-                        xpath("/individualForm/individualRelationshipToHeadOfHousehold").string(
-                                "head"))
+                        xpath("/individualForm/individualRelationshipToHeadOfHousehold")
+                                .string("2"))
                 .andExpect(xpath("/individualForm/individualPhoneNumber").string("12345678890"))
                 .andExpect(xpath("/individualForm/individualOtherPhoneNumber").string("0987654321"))
                 .andExpect(xpath("/individualForm/individualLanguagePreference").string("English"))
@@ -116,7 +121,43 @@ public class IndividualFormResourceTest {
                 .andExpect(xpath("/individualForm/individualPointOfContactPhoneNumber").string(""))
                 .andExpect(xpath("/individualForm/individualDip").string("12345"))
                 .andExpect(xpath("/individualForm/individualMemberStatus").string("permanent"));
+    }
 
+    @Test
+    public void testRepeatPostIndividualFormXml() throws Exception {
+
+        mockMvc.perform(
+                post("/individualForm").session(session).accept(MediaType.APPLICATION_XML)
+                        .contentType(MediaType.APPLICATION_XML)
+                        .body(INDIVIDUAL_FORM_POST_XML.getBytes())).andExpect(status().isCreated())
+                .andExpect(content().mimeType(MediaType.APPLICATION_XML));
+
+        mockMvc.perform(
+                post("/individualForm").session(session).accept(MediaType.APPLICATION_XML)
+                        .contentType(MediaType.APPLICATION_XML)
+                        .body(INDIVIDUAL_FORM_POST_XML.getBytes())).andExpect(status().isCreated())
+                .andExpect(content().mimeType(MediaType.APPLICATION_XML));
+    }
+
+    @Test
+    public void testPostIndividualFormXmlDataCreated() throws Exception {
+        mockMvc.perform(
+                post("/individualForm").session(session).accept(MediaType.APPLICATION_XML)
+                        .contentType(MediaType.APPLICATION_XML)
+                        .body(INDIVIDUAL_FORM_POST_XML.getBytes())).andExpect(status().isCreated())
+                .andExpect(content().mimeType(MediaType.APPLICATION_XML));
+
+        Individual savedIndividual = genericDao.findByProperty(Individual.class, "extId",
+                "TWELVE_DIGIT");
+        Assert.assertNotNull(savedIndividual);
+
+        List<Membership> savedMemberships = genericDao.findListByProperty(Membership.class,
+                "individual", savedIndividual);
+        Assert.assertEquals(1, savedMemberships.size());
+
+        List<Residency> savedResidencies = genericDao.findListByProperty(Residency.class,
+                "individual", savedIndividual);
+        Assert.assertEquals(1, savedResidencies.size());
     }
 
     private MockHttpSession getMockHttpSession(String username, String password) throws Exception {
--- webservice/src/main/java/org/openhds/webservice/resources/bioko/IndividualFormResource.java
+++ webservice/src/main/java/org/openhds/webservice/resources/bioko/IndividualFormResource.java
@@ -1,12 +1,28 @@
 package org.openhds.webservice.resources.bioko;
 
 import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.Calendar;
 
+import org.openhds.controller.exception.ConstraintViolations;
+import org.openhds.controller.service.EntityService;
+import org.openhds.controller.service.FieldWorkerService;
+import org.openhds.controller.service.IndividualService;
 import org.openhds.controller.service.LocationHierarchyService;
 import org.openhds.controller.service.MembershipService;
 import org.openhds.controller.service.RelationshipService;
+import org.openhds.controller.service.ResidencyService;
+import org.openhds.controller.service.SocialGroupService;
+import org.openhds.domain.model.FieldWorker;
+import org.openhds.domain.model.Individual;
+import org.openhds.domain.model.Location;
+import org.openhds.domain.model.Membership;
+import org.openhds.domain.model.Relationship;
+import org.openhds.domain.model.Residency;
+import org.openhds.domain.model.SocialGroup;
 import org.openhds.domain.model.bioko.IndividualForm;
 import org.openhds.webservice.FieldBuilder;
+import org.openhds.webservice.WebServiceCallException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,31 +36,192 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/individualForm")
 public class IndividualFormResource {
-	private static final Logger logger = LoggerFactory
-			.getLogger(IndividualFormResource.class);
+    private static final Logger logger = LoggerFactory.getLogger(IndividualFormResource.class);
 
-	private final FieldBuilder fieldBuilder;
-	private final LocationHierarchyService locationHierarchyService;
-	private final MembershipService membershipService;
-	private final RelationshipService relationshipService;
+    private final FieldWorkerService fieldWorkerService;
+    private final EntityService entityService;
+    private final IndividualService individualService;
+    private final LocationHierarchyService locationHierarchyService;
+    private final ResidencyService residencyService;
+    private final SocialGroupService socialGroupService;
+    private final RelationshipService relationshipService;
+    private final FieldBuilder fieldBuilder;
 
-	@Autowired
-	public IndividualFormResource(
-			LocationHierarchyService locationHierarchyService,
-			MembershipService membershipService,
-			RelationshipService relationshipService, FieldBuilder fieldBuilder) {
-		this.locationHierarchyService = locationHierarchyService;
-		this.membershipService = membershipService;
-		this.relationshipService = relationshipService;
-		this.fieldBuilder = fieldBuilder;
-	}
+    @Autowired
+    public IndividualFormResource(FieldWorkerService fieldWorkerService,
+            EntityService entityService, IndividualService individualService,
+            LocationHierarchyService locationHierarchyService, ResidencyService residencyService,
+            SocialGroupService socialGroupService, RelationshipService relationshipService,
+            FieldBuilder fieldBuilder) {
+        this.fieldWorkerService = fieldWorkerService;
+        this.entityService = entityService;
+        this.individualService = individualService;
+        this.locationHierarchyService = locationHierarchyService;
+        this.residencyService = residencyService;
+        this.socialGroupService = socialGroupService;
+        this.relationshipService = relationshipService;
+        this.fieldBuilder = fieldBuilder;
+    }
 
-	@RequestMapping(method = RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
-	public ResponseEntity<? extends Serializable> processForm(
-			@RequestBody IndividualForm individualForm) {
+    @RequestMapping(method = RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
+    public ResponseEntity<? extends Serializable> processForm(
+            @RequestBody IndividualForm individualForm) {
 
-		return new ResponseEntity<IndividualForm>(individualForm,
-				HttpStatus.CREATED);
-	}
+        ConstraintViolations cv = new ConstraintViolations();
 
+        FieldWorker collectedBy;
+        try {
+            collectedBy = fieldWorkerService.findFieldWorkerById(
+                    individualForm.getFieldWorkerExtId(),
+                    "Individual form has nonexistent field worker id.");
+        } catch (Exception e) {
+            WebServiceCallException error = new WebServiceCallException();
+            error.getErrors().add(e.toString());
+            return new ResponseEntity<WebServiceCallException>(error, HttpStatus.BAD_REQUEST);
+        }
+
+        // the actual individual
+        Individual individual = makeIndividual(individualForm, collectedBy, cv);
+        if (cv.hasViolations()) {
+            return new ResponseEntity<WebServiceCallException>(new WebServiceCallException(cv),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        Calendar collectionTime = Calendar.getInstance();
+        collectionTime.setTime(individualForm.getCollectionDateTime());
+
+        // individual's household social group
+        SocialGroup socialGroup;
+        try {
+            socialGroup = socialGroupService.findSocialGroupById(
+                    individualForm.getHouseholdExtId(),
+                    "Individual form has nonexistent household social group.");
+        } catch (Exception e) {
+            WebServiceCallException error = new WebServiceCallException();
+            error.getErrors().add(e.toString());
+            return new ResponseEntity<WebServiceCallException>(error, HttpStatus.BAD_REQUEST);
+        }
+
+        // individual's membership in the social group
+        // TODO: handle multiple or expired memberships
+        // TODO: this might handled like residencies
+        if (individual.getAllMemberships().size() == 0) {
+            Membership membership = new Membership();
+            membership.setIndividual(individual);
+            membership.setSocialGroup(socialGroup);
+            membership.setCollectedBy(collectedBy);
+            membership.setStartDate(collectionTime);
+            membership.setStartType("Census Individual Form");
+            membership.setbIsToA(individualForm.getIndividualRelationshipToHeadOfHousehold());
+            individual.getAllMemberships().add(membership);
+        }
+
+        // individual's household location
+        Location location = locationHierarchyService.findLocationById(individualForm
+                .getHouseholdExtId());
+        if (null == location) {
+            WebServiceCallException error = new WebServiceCallException();
+            error.getErrors().add("Individual form has nonexistent household location.");
+            return new ResponseEntity<WebServiceCallException>(error, HttpStatus.BAD_REQUEST);
+        }
+
+        // individual's residency at location
+        if (!residencyService.hasOpenResidency(individual)) {
+            Residency residency = residencyService.createResidency(individual, location,
+                    collectionTime, "Census Individual Form", collectedBy);
+            try {
+                residency = residencyService.evaluateResidency(residency);
+                individual.getAllResidencies().add(residency);
+            } catch (ConstraintViolations e) {
+                return new ResponseEntity<WebServiceCallException>(new WebServiceCallException(e),
+                        HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        // TODO: are relationships only for marital relationships?
+
+        try {
+            createOrSaveIndividual(individual);
+        } catch (ConstraintViolations e) {
+            return new ResponseEntity<WebServiceCallException>(new WebServiceCallException(e),
+                    HttpStatus.BAD_REQUEST);
+        } catch (SQLException e) {
+            WebServiceCallException error = new WebServiceCallException();
+            error.getErrors().add("SQLException updating or saving individual: " + e);
+            return new ResponseEntity<WebServiceCallException>(error, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            WebServiceCallException error = new WebServiceCallException();
+            error.getErrors().add("General Exception updating or saving individual: " + e);
+            return new ResponseEntity<WebServiceCallException>(error, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<IndividualForm>(individualForm, HttpStatus.CREATED);
+    }
+
+    private void copyFormDataToIndividual(IndividualForm individualForm, Individual individual) {
+        individual.setExtId(individualForm.getIndividualExtId());
+        individual.setFirstName(individualForm.getIndividualFirstName());
+        individual.setMiddleName(individualForm.getIndividualOtherNames());
+        individual.setLastName(individualForm.getIndividualLastName());
+        individual.setGender(individualForm.getIndividualGender());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(individualForm.getIndividualDateOfBirth());
+        individual.setDob(cal);
+
+        // TODO: add these to the individual data model
+        // private int individualAge;
+        // private String individualAgeUnits;
+        // private String individualPhoneNumber;
+        // private String individualOtherPhoneNumber;
+        // private String individualLanguagePreference;
+        // private String individualPointOfContactName;
+        // private String individualPointOfContactPhoneNumber;
+        // private int individualDip;
+        // private String individualMemberStatus;
+    }
+
+    private Individual getUnknownParent(String gender) {
+        Individual parent = new Individual();
+        parent.setGender(gender);
+        parent.setExtId("UNK");
+
+        Calendar dob = Calendar.getInstance();
+        dob.set(1900, 0, 1);
+        parent.setDob(dob);
+
+        return parent;
+    }
+
+    private Individual makeIndividual(IndividualForm individualForm, FieldWorker collectedBy,
+            ConstraintViolations cv) {
+        Individual individual = individualService
+                .findIndivById(individualForm.getIndividualExtId());
+        if (null == individual) {
+            individual = new Individual();
+        }
+
+        copyFormDataToIndividual(individualForm, individual);
+
+        collectedBy = new FieldWorker();
+        collectedBy.setExtId(individualForm.getFieldWorkerExtId());
+        individual.setCollectedBy(fieldBuilder.referenceField(collectedBy, cv));
+
+        // Bioko project forms don't include parents!
+        Individual mother = getUnknownParent("F");
+        individual.setMother(fieldBuilder.referenceField(mother, cv, "Using Unknown Mother"));
+        Individual father = getUnknownParent("M");
+        individual.setFather(fieldBuilder.referenceField(father, cv, "Using Unknown Father"));
+
+        return individual;
+    }
+
+    private void createOrSaveIndividual(Individual individual) throws ConstraintViolations,
+            SQLException {
+        boolean isUpdate = null != individualService.findIndivById(individual.getExtId());
+        if (isUpdate) {
+            entityService.save(individual);
+        } else {
+            individualService.createIndividual(individual);
+        }
+    }
 }
