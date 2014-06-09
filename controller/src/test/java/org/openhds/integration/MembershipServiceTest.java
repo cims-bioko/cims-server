d149597b9236a0a45cb71e5afca8b31256c47059
Ben Heasly <benjamin.heasly@gmail.com>
2014-06-09 19:46:32 -0400

    testing config and context for controller

---
+++ controller/src/test/java/org/openhds/integration/MembershipServiceTest.java
@@ -0,0 +1,43 @@
+package org.openhds.integration;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openhds.dao.service.GenericDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations={"/controller-test-context.xml"})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
+@DatabaseSetup(value = "/membershipServiceDb.xml", type = DatabaseOperation.REFRESH)
+public class MembershipServiceTest {
+
+    @Autowired
+    private GenericDao genericDao;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testNothing() throws Exception {
+
+    }
+}
