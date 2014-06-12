7b0748ae8ca455e78b6288d5f56a0e044fafd6a7
Ben Heasly <benjamin.heasly@gmail.com>
2014-06-12 18:53:49 -0400

    New IT for MembershipService

--- controller/src/test/java/org/openhds/integration/MembershipServiceTest.java
+++ controller/src/test/java/org/openhds/integration/MembershipServiceTest.java
@@ -6,7 +6,10 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openhds.controller.service.CurrentUser;
+import org.openhds.controller.service.MembershipService;
 import org.openhds.dao.service.GenericDao;
+import org.openhds.domain.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -16,7 +19,10 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
@@ -31,13 +37,68 @@ public class MembershipServiceTest {
     @Autowired
     private GenericDao genericDao;
 
+    @Autowired
+    private MembershipService membershipService;
+
+    @Autowired
+    private CurrentUser currentUser;
+
+    private Individual individual1;
+
+    private Individual individual2;
+
+    private SocialGroup socialGroup1;
+
+    private SocialGroup socialGroup2;
+
+    private FieldWorker fieldWorker;
+
     @Before
     public void setUp() throws Exception {
+        individual1 = genericDao.findByProperty(Individual.class, "extId", "individual1");
+        assertNotNull(individual1);
 
+        individual2 = genericDao.findByProperty(Individual.class, "extId", "individual2");
+        assertNotNull(individual2);
+
+        socialGroup1 = genericDao.findByProperty(SocialGroup.class, "extId", "sg123456789");
+        assertNotNull(socialGroup1);
+
+        socialGroup2 = genericDao.findByProperty(SocialGroup.class, "extId", "sg234567890");
+        assertNotNull(socialGroup2);
+
+        fieldWorker = genericDao.findByProperty(FieldWorker.class, "extId", "UNK");
+        assertNotNull(fieldWorker);
+
+        currentUser.setProxyUser("admin", "test",
+                new String[] {PrivilegeConstants.CREATE_ENTITY, PrivilegeConstants.VIEW_ENTITY});
     }
 
     @Test
-    public void testNothing() throws Exception {
+    public void testGetExistingMembership() throws Exception {
+        List<Membership> membershipList = membershipService.getAllMemberships(individual1);
+        assertNotNull(membershipList);
+        assertEquals(1, membershipList.size());
 
+        Membership membership = membershipList.get(0);
+        assertEquals(individual1, membership.getIndividual());
+    }
+
+    @Test
+    public void testCreateMembership() throws Exception {
+        Membership membership = new Membership();
+        membership.setIndividual(individual2);
+        membership.setSocialGroup(socialGroup1);
+        membership.setCollectedBy(fieldWorker);
+
+        membershipService.evaluateMembership(membership);
+        membershipService.createMembership(membership);
+
+        List<Membership> membershipList = membershipService.getAllMemberships(individual2);
+        assertNotNull(membershipList);
+        assertEquals(1, membershipList.size());
+
+        Membership savedMembership = membershipList.get(0);
+        assertEquals(individual2, savedMembership.getIndividual());
     }
 }
