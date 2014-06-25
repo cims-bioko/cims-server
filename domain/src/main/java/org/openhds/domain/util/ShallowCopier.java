dde8efd2dcf23d9c0ef1e3bf1acac7a534174555
Ben Heasly <benjamin.heasly@gmail.com>
2014-06-25 14:10:06 -0400

    ResidencyXmlWriteTaskTest inspects values in written XML.

--- domain/src/main/java/org/openhds/domain/util/ShallowCopier.java
+++ domain/src/main/java/org/openhds/domain/util/ShallowCopier.java
@@ -1,114 +1,108 @@
 package org.openhds.domain.util;
 
-import org.openhds.domain.model.FieldWorker;
-import org.openhds.domain.model.Individual;
-import org.openhds.domain.model.Location;
-import org.openhds.domain.model.LocationHierarchy;
-import org.openhds.domain.model.Membership;
-import org.openhds.domain.model.Relationship;
-import org.openhds.domain.model.Residency;
-import org.openhds.domain.model.SocialGroup;
-import org.openhds.domain.model.Visit;
+import org.openhds.domain.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ShallowCopier {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(ShallowCopier.class);
-	
-	public static Residency shallowCopyResidency(Residency residency) {
-		Residency copy = new Residency();
-		copy.setCollectedBy(residency.getCollectedBy());
-		Individual individualStub = Individual.makeStub(residency.getIndividual().getExtId());
-		copy.setIndividual(individualStub);
-		Location locationStub = Location.makeStub(residency.getLocation().getExtId());
-		copy.setLocation(locationStub);
-		return copy;
-	}
-	
-	public static Membership shallowCopyMembership(Membership membership) {
-		Membership copy = new Membership();
-		copy.setbIsToA(membership.getbIsToA());
+    private static final Logger logger = LoggerFactory
+            .getLogger(ShallowCopier.class);
+
+    public static Residency shallowCopyResidency(Residency residency) {
+        Residency copy = new Residency();
+        copy.setCollectedBy(residency.getCollectedBy());
+        Individual individualStub = Individual.makeStub(residency.getIndividual().getExtId());
+        copy.setIndividual(individualStub);
+        Location locationStub = Location.makeStub(residency.getLocation().getExtId());
+        copy.setLocation(locationStub);
+        copy.setStartDate(residency.getStartDate());
+        copy.setStartType(residency.getStartType());
+        return copy;
+    }
+
+    public static Membership shallowCopyMembership(Membership membership) {
+        Membership copy = new Membership();
+        copy.setbIsToA(membership.getbIsToA());
         FieldWorker fieldworkerStub = FieldWorker.makeStub(membership.getCollectedBy().getExtId());
         copy.setCollectedBy(fieldworkerStub);
-		Individual individualStub = Individual.makeStub(membership.getIndividual().getExtId());
-		copy.setIndividual(individualStub);
-		SocialGroup sgStub = SocialGroup.makeStub(membership.getSocialGroup().getExtId());
-		copy.setSocialGroup(sgStub);
-		return copy;
-	}
+        Individual individualStub = Individual.makeStub(membership.getIndividual().getExtId());
+        copy.setIndividual(individualStub);
+        SocialGroup sgStub = SocialGroup.makeStub(membership.getSocialGroup().getExtId());
+        copy.setSocialGroup(sgStub);
+        return copy;
+    }
 
-	public static Individual shallowCopyIndividual(Individual individual) {
+    public static Individual shallowCopyIndividual(Individual individual) {
 
-		Individual copy = new Individual();
-		try {
-			copy.setDob(individual.getDob());
-			copy.setExtId(individual.getExtId());
+        Individual copy = new Individual();
+        try {
+            copy.setDob(individual.getDob());
+            copy.setExtId(individual.getExtId());
 
-			copy.setFather(copyExtId(individual.getFather()));
-			copy.setFirstName(individual.getFirstName());
-			copy.setGender(individual.getGender());
-			copy.setLastName(individual.getLastName());
-			String middleName = individual.getMiddleName() == null ? ""
-					: individual.getMiddleName();
-			copy.setMiddleName(middleName);
-			copy.setMother(copyExtId(individual.getMother()));
+            copy.setFather(copyExtId(individual.getFather()));
+            copy.setFirstName(individual.getFirstName());
+            copy.setGender(individual.getGender());
+            copy.setLastName(individual.getLastName());
+            String middleName = individual.getMiddleName() == null ? ""
+                    : individual.getMiddleName();
+            copy.setMiddleName(middleName);
+            copy.setMother(copyExtId(individual.getMother()));
 
-			copy.setAge(individual.getAge());
-			copy.setAgeUnits(individual.getAgeUnits());
-			copy.setPhoneNumber(individual.getPhoneNumber());
-			copy.setOtherPhoneNumber(individual.getOtherPhoneNumber());
-			copy.setLanguagePreference(individual.getLanguagePreference());
-			copy.setPointOfContactName(individual.getPointOfContactName());
-			copy.setPointOfContactPhoneNumber(individual
-					.getPointOfContactPhoneNumber());
-			copy.setDip(individual.getDip());
-			copy.setMemberStatus(individual.getMemberStatus());
+            copy.setAge(individual.getAge());
+            copy.setAgeUnits(individual.getAgeUnits());
+            copy.setPhoneNumber(individual.getPhoneNumber());
+            copy.setOtherPhoneNumber(individual.getOtherPhoneNumber());
+            copy.setLanguagePreference(individual.getLanguagePreference());
+            copy.setPointOfContactName(individual.getPointOfContactName());
+            copy.setPointOfContactPhoneNumber(individual
+                    .getPointOfContactPhoneNumber());
+            copy.setDip(individual.getDip());
+            copy.setMemberStatus(individual.getMemberStatus());
 
-			Individual individualStub = Individual.makeStub(individual.getExtId());
+            Individual individualStub = Individual.makeStub(individual.getExtId());
 
-			for (Membership membership : individual.getAllMemberships()) {
-				SocialGroup socialGroupStub = SocialGroup.makeStub(membership
-						.getSocialGroup().getExtId());
-				Membership membershipStub = Membership
-						.makeStub(socialGroupStub, individualStub,
-								membership.getbIsToA());
-				copy.getAllMemberships().add(membershipStub);
-			}
+            for (Membership membership : individual.getAllMemberships()) {
+                SocialGroup socialGroupStub = SocialGroup.makeStub(membership
+                        .getSocialGroup().getExtId());
+                Membership membershipStub = Membership
+                        .makeStub(socialGroupStub, individualStub,
+                                membership.getbIsToA());
+                copy.getAllMemberships().add(membershipStub);
+            }
 
-			Residency currentResidency = individual.getCurrentResidency();
-			if (null != currentResidency) {
-				Location locationStub = Location.makeStub(currentResidency.getLocation().getExtId());
-				locationStub.setLocationLevel(null);
-				Residency residencyStub = Residency.makeStub(locationStub,individualStub);
-				copy.getAllResidencies().add(residencyStub);
-			}
-		} catch (Exception e) {
-			System.out.println(copy.getExtId());
-		}
-		return copy;
-	}
+            Residency currentResidency = individual.getCurrentResidency();
+            if (null != currentResidency) {
+                Location locationStub = Location.makeStub(currentResidency.getLocation().getExtId());
+                locationStub.setLocationLevel(null);
+                Residency residencyStub = Residency.makeStub(locationStub,individualStub);
+                copy.getAllResidencies().add(residencyStub);
+            }
+        } catch (Exception e) {
+            System.out.println(copy.getExtId());
+        }
+        return copy;
+    }
 
-	private static Individual copyExtId(Individual individual) {
-		Individual copy = new Individual();
-		if (individual == null) {
-			logger.warn("Individual had a null father or mother - using UNK as default value");
-			copy.setExtId("UNK");
-		} else {
-			copy.setExtId(individual.getExtId());
-		}
+    private static Individual copyExtId(Individual individual) {
+        Individual copy = new Individual();
+        if (individual == null) {
+            logger.warn("Individual had a null father or mother - using UNK as default value");
+            copy.setExtId("UNK");
+        } else {
+            copy.setExtId(individual.getExtId());
+        }
 
-		return copy;
-	}
+        return copy;
+    }
 
-	public static Location copyLocation(Location loc) {
-		Location copy = new Location();
+    public static Location copyLocation(Location loc) {
+        Location copy = new Location();
 
-		copy.setAccuracy(getEmptyStringIfBlank(loc.getAccuracy()));
-		copy.setAltitude(getEmptyStringIfBlank(loc.getAltitude()));
-		copy.setLatitude(getEmptyStringIfBlank(loc.getLatitude()));
-		copy.setLongitude(getEmptyStringIfBlank(loc.getLongitude()));
+        copy.setAccuracy(getEmptyStringIfBlank(loc.getAccuracy()));
+        copy.setAltitude(getEmptyStringIfBlank(loc.getAltitude()));
+        copy.setLatitude(getEmptyStringIfBlank(loc.getLatitude()));
+        copy.setLongitude(getEmptyStringIfBlank(loc.getLongitude()));
 
         // extensions for bioko island project
         copy.setSectorName(getEmptyStringIfBlank(loc.getSectorName()));
@@ -116,63 +110,63 @@ public class ShallowCopier {
         copy.setCommunityName(getEmptyStringIfBlank(loc.getCommunityName()));
 
         LocationHierarchy level = new LocationHierarchy();
-		level.setExtId(loc.getLocationLevel().getExtId());
-		copy.setLocationLevel(level);
+        level.setExtId(loc.getLocationLevel().getExtId());
+        copy.setLocationLevel(level);
 
-		copy.setExtId(loc.getExtId());
-		copy.setLocationName(loc.getLocationName());
-		copy.setLocationType(loc.getLocationType());
+        copy.setExtId(loc.getExtId());
+        copy.setLocationName(loc.getLocationName());
+        copy.setLocationType(loc.getLocationType());
 
-		FieldWorker fieldworkerStub = FieldWorker.makeStub(loc.getCollectedBy().getExtId());
-		copy.setCollectedBy(fieldworkerStub);
-		return copy;
-	}
+        FieldWorker fieldworkerStub = FieldWorker.makeStub(loc.getCollectedBy().getExtId());
+        copy.setCollectedBy(fieldworkerStub);
+        return copy;
+    }
 
-	private static String getEmptyStringIfBlank(String accuracy) {
-		if (accuracy == null || accuracy.trim().isEmpty()) {
-			return "";
-		}
+    private static String getEmptyStringIfBlank(String accuracy) {
+        if (accuracy == null || accuracy.trim().isEmpty()) {
+            return "";
+        }
 
-		return accuracy;
-	}
+        return accuracy;
+    }
 
-	public static Relationship copyRelationship(Relationship original) {
-		Relationship copy = new Relationship();
-		copy.setaIsToB(original.getaIsToB());
+    public static Relationship copyRelationship(Relationship original) {
+        Relationship copy = new Relationship();
+        copy.setaIsToB(original.getaIsToB());
 
-		Individual individualStub = Individual.makeStub(original.getIndividualA().getExtId());
-		copy.setIndividualA(individualStub);
+        Individual individualStub = Individual.makeStub(original.getIndividualA().getExtId());
+        copy.setIndividualA(individualStub);
 
-		individualStub = Individual.makeStub(original.getIndividualB().getExtId());
-		copy.setIndividualB(individualStub);
+        individualStub = Individual.makeStub(original.getIndividualB().getExtId());
+        copy.setIndividualB(individualStub);
 
-		copy.setStartDate(original.getStartDate());
-		return copy;
-	}
+        copy.setStartDate(original.getStartDate());
+        return copy;
+    }
 
-	public static SocialGroup copySocialGroup(SocialGroup original) {
-		Individual groupHeadStub = Individual.makeStub(original.getGroupHead().getExtId());
-		SocialGroup copy = new SocialGroup();
-		copy.setExtId(original.getExtId());
-		copy.setGroupHead(groupHeadStub);
-		copy.setGroupName(original.getGroupName());
-		return copy;
-	}
+    public static SocialGroup copySocialGroup(SocialGroup original) {
+        Individual groupHeadStub = Individual.makeStub(original.getGroupHead().getExtId());
+        SocialGroup copy = new SocialGroup();
+        copy.setExtId(original.getExtId());
+        copy.setGroupHead(groupHeadStub);
+        copy.setGroupName(original.getGroupName());
+        return copy;
+    }
 
-	public static Visit copyVisit(Visit original) {
-		FieldWorker fieldworkerStub = FieldWorker.makeStub(original.getCollectedBy().getExtId());
-		Location locationStub = Location.makeStub(original.getVisitLocation().getExtId());
-		locationStub.setLocationLevel(null);
+    public static Visit copyVisit(Visit original) {
+        FieldWorker fieldworkerStub = FieldWorker.makeStub(original.getCollectedBy().getExtId());
+        Location locationStub = Location.makeStub(original.getVisitLocation().getExtId());
+        locationStub.setLocationLevel(null);
 
-		Visit copy = new Visit();
-		copy.setCollectedBy(fieldworkerStub);
-		copy.setVisitLocation(locationStub);
-		copy.setExtId(original.getExtId());
-		copy.setRoundNumber(original.getRoundNumber());
-		copy.setVisitDate(original.getVisitDate());
+        Visit copy = new Visit();
+        copy.setCollectedBy(fieldworkerStub);
+        copy.setVisitLocation(locationStub);
+        copy.setExtId(original.getExtId());
+        copy.setRoundNumber(original.getRoundNumber());
+        copy.setVisitDate(original.getVisitDate());
 
-		return copy;
-	}
+        return copy;
+    }
 
     public static Membership copyMembership(Membership membership) {
         Membership copy = new Membership();
--- task/src/test/java/org/openhds/task/ResidencyXmlWriterTaskTest.java
+++ task/src/test/java/org/openhds/task/ResidencyXmlWriterTaskTest.java
@@ -1,9 +1,8 @@
 package org.openhds.task;
 
-import static org.mockito.MockitoAnnotations.initMocks;
-
-import java.io.File;
-
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +11,6 @@ import org.openhds.controller.service.ResidencyService;
 import org.openhds.domain.model.PrivilegeConstants;
 import org.openhds.task.service.AsyncTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,47 +18,67 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseOperation;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXpathEvaluatesTo;
+import static org.custommonkey.xmlunit.XMLAssert.assertXpathExists;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@Transactional
 @ContextConfiguration(locations={"/task-test-context.xml"})
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class,
-    TransactionalTestExecutionListener.class,
-    DbUnitTestExecutionListener.class })
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
 @DatabaseSetup(value = "/taskTestDb.xml", type = DatabaseOperation.REFRESH)
 public class ResidencyXmlWriterTaskTest {
-	
-	@Autowired
-	private AsyncTaskService asyncTaskService;
-	
-	@Autowired
-	private ResidencyService residencyService;
-	
-	@Autowired
-	private CurrentUser currentUser;
-	
-	@Before
-	public void setUp() {
-		initMocks(this);
+
+    @Autowired
+    private AsyncTaskService asyncTaskService;
+
+    @Autowired
+    private ResidencyService residencyService;
+
+    @Autowired
+    private CurrentUser currentUser;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
         currentUser.setProxyUser("admin", "test",
-                new String[] {PrivilegeConstants.CREATE_ENTITY, PrivilegeConstants.VIEW_ENTITY});
-	}
-	
-	@Test
-	public void shouldWriteXml() {
-		
-		ResidencyXmlWriterTask task = new ResidencyXmlWriterTask(asyncTaskService, residencyService);
-		File fileToWrite = new File("residencies.xml");
-		TaskContext context = new TaskContext(fileToWrite);
-		task.writeXml(context);
+                new String[]{PrivilegeConstants.CREATE_ENTITY, PrivilegeConstants.VIEW_ENTITY});
+    }
 
-	}
-	
+    @Test
+    public void shouldWriteXml() {
 
-	
+        File fileToWrite = new File("residencies-test.xml");
 
+        try {
+            ResidencyXmlWriterTask task = new ResidencyXmlWriterTask(asyncTaskService, residencyService);
+            TaskContext context = new TaskContext(fileToWrite);
+            task.writeXml(context);
+
+            assertTrue(fileToWrite.exists());
+            String xmlWritten = new String(Files.readAllBytes(Paths.get(fileToWrite.getPath())));
+            assertXpathExists("/residencies", xmlWritten);
+            assertXpathExists("/residencies/residency", xmlWritten);
+            assertXpathExists("/residencies/residency/startDate", xmlWritten);
+            assertXpathExists("/residencies/residency/startType", xmlWritten);
+            assertXpathEvaluatesTo("individual1", "/residencies/residency/individual/extId", xmlWritten);
+            assertXpathEvaluatesTo("testLocation", "/residencies/residency/location/extId", xmlWritten);
+
+        } catch (Exception e) {
+            fail("Problem testing Residency XML: " + e.getMessage());
+
+        } finally {
+            if (fileToWrite.exists()) {
+                fileToWrite.delete();
+            }
+        }
+    }
 }
