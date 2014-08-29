63a22175c09b3de5d3d92b54995b9b1a63c40b6a
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-08-29 14:14:19 -0400

    Syntax correction in .js script

--- web/src/main/webapp/errors/scripts/ng/services.js
+++ web/src/main/webapp/errors/scripts/ng/services.js
@@ -3,6 +3,6 @@ var errorServices = angular.module('errorServices', ['ngResource']);
 errorServices.factory('AllUnresolvedErrorsService', ['$resource',
 function($resource){
         return $resource('/openhds/api/rest/errors.json', {}, {
-            query: {method:'GET', params:{resolution, 'Unresolved'}},
+            query: {method:'GET', params:{resolution:'Unresolved'}},
             });
 	}]);
