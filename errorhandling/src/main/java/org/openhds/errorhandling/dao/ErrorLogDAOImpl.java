df0ae4dfa64a301ee16bceb35353f18114d44904
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-02 15:33:04 -0400

    Calendar instance should be formatted before being set in the errorlog

--- errorhandling/src/main/java/org/openhds/errorhandling/dao/ErrorLogDAOImpl.java
+++ errorhandling/src/main/java/org/openhds/errorhandling/dao/ErrorLogDAOImpl.java
@@ -2,6 +2,7 @@ package org.openhds.errorhandling.dao;
 
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.openhds.dao.service.GenericDao.ValueProperty;
 import org.openhds.dao.service.GenericDao.RangeProperty;
@@ -10,14 +11,19 @@ import org.openhds.controller.service.EntityService;
 import org.openhds.dao.service.GenericDao;
 import org.openhds.domain.model.FieldWorker;
 import org.openhds.domain.model.ErrorLog;
+import org.openhds.domain.util.CalendarUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ErrorLogDAOImpl implements ErrorLogDAO {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorLogDAOImpl.class);
     private EntityService entityService;
     private GenericDao genericDao;
+
+    @Autowired
+    private CalendarUtil calendarUtil;
 
     public ErrorLogDAOImpl(EntityService entityService, GenericDao genericDao) {
         this.entityService = entityService;
@@ -26,7 +32,8 @@ public class ErrorLogDAOImpl implements ErrorLogDAO {
 
     public ErrorLog createErrorLog(ErrorLog error) {
         try {
-            error.setInsertDate(Calendar.getInstance());
+            Calendar insertDate = calendarUtil.convertDateToCalendar(new Date());
+            error.setInsertDate(insertDate);
             entityService.create(error);
         } catch (IllegalArgumentException e) {
             logger.warn("Unable to insert error log into DB: " + e.getMessage());
