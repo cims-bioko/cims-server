afa208ec7c6c3ca9a6e39faa5e28ef9f8e97ff54
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-02 11:05:47 -0400

    Fix the angular routing

--- web/src/main/webapp/errors/scripts/ng/app.js
+++ web/src/main/webapp/errors/scripts/ng/app.js
@@ -4,6 +4,6 @@ var myApp = angular.module('myApp', ['ngRoute','errorServices','errorControllers
 
 myApp.config(['$routeProvider',
     function($routeProvider) {
-        $routeProvider.when('errors/all/unresolved', {templateUrl: 'errors/partials/errors-all-unresolved.html', controller: 'AllUnresolvedErrorsController'})
+        $routeProvider.when('/all/unresolved', {templateUrl: 'errors/partials/errors-all-unresolved.html', controller: 'AllUnresolvedErrorsController'})
         .otherwise({redirectTo: 'partials/home.html'});
 }]);
