239329f9873883b7bb072e8a101d7e5901876266
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-02 09:46:06 -0400

    Typo in angular error controller

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -3,4 +3,4 @@ var errorControllers = angular.module('errorControllers',[]);
 
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
-            $scope.allUnresolvedErrors - AllUnresolvedErrorsService.query();}]);
+            $scope.allUnresolvedErrors = AllUnresolvedErrorsService.query();}]);
