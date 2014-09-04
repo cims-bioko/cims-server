d7843ee7e7a6afc953460b45abf45dfbbff3c6e2
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-04 14:15:15 -0400

    Insert dates in epoch time must be converted to a readable form

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -3,4 +3,13 @@ var errorControllers = angular.module('errorControllers',[]);
 
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
-            $scope.allUnresolvedErrors = AllUnresolvedErrorsService.query();}]);
+            var errorLogArray = AllUnresolvedErrorsService.query();
+
+            for (var i = 0; i < errorLogArray.length; i++) {
+                var errLogJsonObject = errorLogArray[i];
+                var insertDate = new Date(errLogJsonObject['insertDate']);
+                var insertDateString = insertDate.toString();
+                errLogJsonObject['insertDate'] = insertDateString;
+            }
+
+            $scope.allUnresolvedErrors = errorLogArray;}]);
