c3f646c3f9b52bc6cdb6aaffcfe8296f0c7f68df
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-04 14:46:42 -0400

    More bug fixes in error UI

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -3,14 +3,16 @@ var errorControllers = angular.module('errorControllers',[]);
 
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
-            var data = AllUnresolvedErrorsService.query();
-            var errorLogArray = data['errors'];
+            var jsonResponse = AllUnresolvedErrorsService.query();
 
-            for (var i = 0; i < errorLogArray.length; i++) {
-                var errLogJsonObject = errorLogArray[i];
-                var insertDate = new Date(errLogJsonObject['insertDate']);
-                var insertDateString = insertDate.toString();
-                errLogJsonObject['insertDate'] = insertDateString;
+            //convert insertDate from epoch time string to JS date string
+            if (jsonResponse.data.errors.length > 0) {
+                        for (var i = 0; i < jsonResponse.data.errors.length; i++) {
+                            var insertDateEpochTime = jsonResponse.data.errors[i].insertDate;
+                            var insertDate = new Date(insertDateEpochTime);
+                            var insertDateString = insertDate.toString();
+                            jsonResponse.data.errors[i].insertDate = insertDateString;
+                        }
             }
 
-            $scope.allUnresolvedErrors = errorLogArray;}]);
+            $scope.allUnresolvedErrors = jsonResponse;}]);
