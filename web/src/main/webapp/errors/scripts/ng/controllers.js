9812463d927f1be937d09a8f5a0b432984ef748c
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-15 10:10:54 -0400

    Use JSON.parse in the ErrorReview controller

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -4,15 +4,18 @@ var errorControllers = angular.module('errorControllers',[]);
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
             var jsonResponse = AllUnresolvedErrorsService.query();
+                var obj = JSON.parse(jsonResponse);
+                $scope.resultMessage = obj.resultMessage;
+                var data = obj.data;
 
             //convert insertDate from epoch time string to JS date string
-            if (jsonResponse.data.errors.length > 0) {
-                        for (var i = 0; i < jsonResponse.data.errors.length; i++) {
-                            var insertDateEpochTime = jsonResponse.data.errors[i].insertDate;
+            if (data.errors.length > 0) {
+                        for (var i = 0; i < data.errors.length; i++) {
+                            var insertDateEpochTime = data.errors[i].insertDate;
                             var insertDate = new Date(insertDateEpochTime);
                             var insertDateString = insertDate.toString();
-                            jsonResponse.data.errors[i].insertDate = insertDateString;
+                            data.errors[i].insertDate = insertDateString;
                         }
             }
 
-            $scope.allUnresolvedErrors = jsonResponse;}]);
+            $scope.allUnresolvedErrors = data;}]);
