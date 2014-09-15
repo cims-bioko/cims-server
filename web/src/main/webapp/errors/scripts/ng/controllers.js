c55c4432b7f9f1782ff36a046e666566eb241d8e
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-15 10:25:18 -0400

    More UI debugging

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -4,18 +4,17 @@ var errorControllers = angular.module('errorControllers',[]);
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
             var jsonResponse = AllUnresolvedErrorsService.query();
-                var obj = JSON.parse(jsonResponse);
-                $scope.resultMessage = obj.resultMessage;
-                var data = obj.data;
+            $scope.resultMessage = jsonResponse.resultMessage;
+            var errors = jsonResponse.data.errors;
 
             //convert insertDate from epoch time string to JS date string
-            if (data.errors.length > 0) {
-                        for (var i = 0; i < data.errors.length; i++) {
-                            var insertDateEpochTime = data.errors[i].insertDate;
+            if (errors.length > 0) {
+                        for (var i = 0; i < errors.length; i++) {
+                            var insertDateEpochTime = errors[i].insertDate;
                             var insertDate = new Date(insertDateEpochTime);
                             var insertDateString = insertDate.toString();
-                            data.errors[i].insertDate = insertDateString;
+                            errors[i].insertDate = insertDateString;
                         }
             }
 
-            $scope.allUnresolvedErrors = data;}]);
+            $scope.allUnresolvedErrors = errors;}]);
