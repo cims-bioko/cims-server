7a80df0a1b6dfe4e80e3b9aad6860e8b40c3305d
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-15 10:58:33 -0400

    Angular services should specify that the JSON response is an array for a getAll query

--- web/src/main/webapp/errors/scripts/ng/controllers.js
+++ web/src/main/webapp/errors/scripts/ng/controllers.js
@@ -3,14 +3,15 @@ var errorControllers = angular.module('errorControllers',[]);
 
 errorControllers.controller('AllUnresolvedErrorsController', ['$scope', 'AllUnresolvedErrorsService',
     function($scope, AllUnresolvedErrorsService) {
-            var jsonResponse = AllUnresolvedErrorsService.query();
-            var jsonObject;
-            jsonResponse.$promise.then(function (result) {
-                jsonObject = result;
-            });
-            $scope.resultMessage = jsonObject.resultMessage;
 
-            var errors = jsonObject.data.errors;
+            $scope.jsonResponse = AllUnresolvedErrorsService.query();
+            $scope.jsonResponse.$promise.then(function (result) {
+                $scope.jsonResponse = result;
+            })
+
+            $scope.resultMessage = $scope.jsonResponse.resultMessage;
+
+            var errors = $scope.jsonResponse.data.errors;
 
             //convert insertDate from epoch time string to JS date string
             if (errors.length > 0) {
