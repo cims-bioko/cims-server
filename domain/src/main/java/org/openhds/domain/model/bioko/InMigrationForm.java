ffbeab34df7b56f9f24bc06bbf658f84b0b7250e
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2014-09-19 14:43:59 -0400

    InMigration form resource and DTO

---
+++ domain/src/main/java/org/openhds/domain/model/bioko/InMigrationForm.java
@@ -0,0 +1,131 @@
+package org.openhds.domain.model.bioko;
+
+import org.openhds.domain.annotations.Description;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.io.Serializable;
+import java.util.Calendar;
+
+@Description(description = "Model data from the InMigration xform for the Bioko island project.")
+@XmlRootElement(name = "inMigrationForm")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class InMigrationForm implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement(name = "processed_by_mirth")
+    private boolean processedByMirth;
+
+    @XmlElement(name = "collection_date_time")
+    @XmlJavaTypeAdapter(org.openhds.domain.util.CalendarAdapter.class)
+    private Calendar collectionDateTime;
+
+    @XmlElement(name = "field_worker_ext_id")
+    private String fieldWorkerExtId;
+
+    @XmlElement(name = "visit_ext_id")
+    private String visitExtId;
+
+    @XmlElement(name = "location_ext_id")
+    private String locationExtId;
+
+    @XmlElement(name = "individual_ext_id")
+    private String individualExtId;
+
+    @XmlElement(name = "migration_type")
+    private String migrationType;
+
+    @XmlElement(name = "migration_date")
+    @XmlJavaTypeAdapter(org.openhds.domain.util.CalendarAdapter.class)
+    private Calendar migrationDate;
+
+    @XmlElement(name = "migration_origin")
+    private String migrationOrigin;
+
+    @XmlElement(name = "migrationReason")
+    private String migrationReason;
+
+    public boolean isProcessedByMirth() {
+        return processedByMirth;
+    }
+
+    public void setProcessedByMirth(boolean processedByMirth) {
+        this.processedByMirth = processedByMirth;
+    }
+
+    public String getFieldWorkerExtId() {
+        return fieldWorkerExtId;
+    }
+
+    public void setFieldWorkerExtId(String fieldWorkerExtId) {
+        this.fieldWorkerExtId = fieldWorkerExtId;
+    }
+
+    public String getVisitExtId() {
+        return visitExtId;
+    }
+
+    public void setVisitExtId(String visitExtId) {
+        this.visitExtId = visitExtId;
+    }
+
+    public String getMigrationType() {
+        return migrationType;
+    }
+
+    public void setMigrationType(String migrationType) {
+        this.migrationType = migrationType;
+    }
+
+    public String getIndividualExtId() {
+        return individualExtId;
+    }
+
+    public void setIndividualExtId(String individualExtId) {
+        this.individualExtId = individualExtId;
+    }
+
+    public Calendar getCollectionDateTime() {
+        return collectionDateTime;
+    }
+
+    public void setCollectionDateTime(Calendar collectionDateTime) {
+        this.collectionDateTime = collectionDateTime;
+    }
+
+    public String getLocationExtId() {
+        return locationExtId;
+    }
+
+    public void setLocationExtId(String locationExtId) {
+        this.locationExtId = locationExtId;
+    }
+
+    public Calendar getMigrationDate() {
+        return migrationDate;
+    }
+
+    public void setMigrationDate(Calendar migrationDate) {
+        this.migrationDate = migrationDate;
+    }
+
+    public String getMigrationOrigin() {
+        return migrationOrigin;
+    }
+
+    public void setMigrationOrigin(String migrationOrigin) {
+        this.migrationOrigin = migrationOrigin;
+    }
+
+    public String getMigrationReason() {
+        return migrationReason;
+    }
+
+    public void setMigrationReason(String migrationReason) {
+        this.migrationReason = migrationReason;
+    }
+}
---
+++ webservice/src/main/java/org/openhds/webservice/resources/bioko/InMigrationFormResource.java
@@ -0,0 +1,146 @@
+package org.openhds.webservice.resources.bioko;
+
+
+import org.openhds.controller.exception.ConstraintViolations;
+import org.openhds.controller.service.*;
+import org.openhds.domain.model.*;
+import org.openhds.domain.model.bioko.InMigrationForm;
+import org.openhds.domain.model.bioko.OutMigrationForm;
+import org.openhds.domain.service.SitePropertiesService;
+import org.openhds.domain.util.CalendarAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.Serializable;
+import java.io.StringWriter;
+
+@Controller
+@RequestMapping("/inMigrationForm")
+public class InMigrationFormResource extends AbstractFormResource {
+
+    @Autowired
+    private IndividualService individualService;
+
+    @Autowired
+    private FieldWorkerService fieldWorkerService;
+
+    @Autowired
+    private VisitService visitService;
+
+    @Autowired
+    private LocationHierarchyService locationService;
+
+    @Autowired
+    private SitePropertiesService sitePropertiesService;
+
+    @Autowired
+    private InMigrationService inMigrationService;
+
+    @Autowired
+    private CalendarAdapter adapter;
+
+    private JAXBContext context = null;
+    private Marshaller marshaller = null;
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
+    @Transactional
+    public ResponseEntity<? extends Serializable> processForm(@RequestBody InMigrationForm inMigrationForm) throws JAXBException {
+
+        try {
+            context = JAXBContext.newInstance(InMigrationForm.class);
+            marshaller = context.createMarshaller();
+            marshaller.setAdapter(adapter);
+        } catch (JAXBException e) {
+            throw new RuntimeException("Could not create JAXB context and marshaller for InMigrationFormResource");
+        }
+
+        InMigration inMigration = new InMigration();
+
+        inMigration.setRecordedDate(inMigrationForm.getMigrationDate());
+        inMigration.setReason(inMigrationForm.getMigrationReason());
+        inMigration.setOrigin(inMigrationForm.getMigrationOrigin());
+
+        //TODO: determine a consistent configuration plan between siteProperties and MigrationType enum
+        //TODO: (question) why use an enum and not the siteproperties?
+        if ("internal_inmigration".equals(inMigrationForm.getMigrationType())) {
+            inMigration.setMigType(MigrationType.INTERNAL_INMIGRATION);
+        } else {
+            inMigration.setMigType(MigrationType.EXTERNAL_INMIGRATION);
+        }
+
+        FieldWorker fieldWorker = fieldWorkerService.findFieldWorkerById(inMigrationForm.getFieldWorkerExtId());
+        if (null == fieldWorker) {
+            ConstraintViolations cv = new ConstraintViolations();
+            cv.addViolations(ConstraintViolations.INVALID_FIELD_WORKER_EXT_ID + ":"+inMigrationForm.getFieldWorkerExtId());
+            logError(cv, fieldWorker, createDTOPayload(inMigrationForm), InMigrationForm.class.getSimpleName());
+            return requestError(cv);
+        }
+        inMigration.setCollectedBy(fieldWorker);
+
+        Visit visit = visitService.findVisitById(inMigrationForm.getVisitExtId());
+        if (null == visit) {
+            ConstraintViolations cv = new ConstraintViolations();
+            cv.addViolations(ConstraintViolations.INVALID_VISIT_EXT_ID + ":"+inMigrationForm.getVisitExtId());
+            logError(cv, fieldWorker, createDTOPayload(inMigrationForm), InMigrationForm.class.getSimpleName());
+            return requestError(cv);
+        }
+        inMigration.setVisit(visit);
+
+        Individual individual = individualService.findIndivById(inMigrationForm.getIndividualExtId());
+        if (null == individual) {
+            ConstraintViolations cv = new ConstraintViolations();
+            cv.addViolations(ConstraintViolations.INVALID_INDIVIDUAL_EXT_ID+":"+inMigrationForm.getIndividualExtId());
+            logError(cv, fieldWorker, createDTOPayload(inMigrationForm), InMigrationForm.class.getSimpleName());
+            return requestError(cv);
+        }
+        inMigration.setIndividual(individual);
+
+        Location location = locationService.findLocationById(inMigrationForm.getLocationExtId());
+        if (null == location) {
+            ConstraintViolations cv = new ConstraintViolations();
+            cv.addViolations(ConstraintViolations.INVALID_LOCATION_EXT_ID+":"+inMigrationForm.getLocationExtId());
+            logError(cv, fieldWorker, createDTOPayload(inMigrationForm), InMigrationForm.class.getSimpleName());
+            return requestError(cv);
+        }
+
+        Residency newResidency = new Residency();
+
+        //TODO: since the InMigration domain model contains a reference to a Residency instead of a Location,
+        //we must assemble the Residency at this level to provide a fully-graphed InMigration to the service
+        newResidency.setLocation(location);
+        newResidency.setIndividual(individual);
+        newResidency.setStartDate(inMigration.getRecordedDate());
+        newResidency.setStartType(sitePropertiesService.getInmigrationCode());
+        newResidency.setEndType(sitePropertiesService.getNotApplicableCode());
+
+        inMigration.setResidency(newResidency);
+
+        try {
+            inMigrationService.createInMigration(inMigration);
+        } catch (ConstraintViolations cv) {
+            logError(cv, fieldWorker, createDTOPayload(inMigrationForm), InMigrationForm.class.getSimpleName());
+            return requestError(cv);
+        }
+
+        return new ResponseEntity<InMigrationForm>(inMigrationForm, HttpStatus.CREATED);
+
+    }
+
+
+    private String createDTOPayload(InMigrationForm inMigrationForm) throws JAXBException {
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(inMigrationForm, writer);
+        return writer.toString();
+    }
+
+
+}
