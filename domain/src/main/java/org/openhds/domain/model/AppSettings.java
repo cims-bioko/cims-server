2ee3a2fbbad7cc048bde6cb053f40b2228287c15
Ben Heasly <benjamin.heasly@gmail.com>
2014-11-06 12:05:56 -0500

    use version name and number from version-tag properties file

--- domain/src/main/java/org/openhds/domain/model/AppSettings.java
+++ domain/src/main/java/org/openhds/domain/model/AppSettings.java
@@ -7,11 +7,22 @@ public class AppSettings {
     @Description(description="Version number of the application.")
     String versionNumber;
 
+    @Description(description="Version name/mnemonic of the application.")
+    String versionName;
+
     public String getVersionNumber() {
         return versionNumber;
     }
 
     public void setVersionNumber(String versionNumber) {
         this.versionNumber = versionNumber;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
     }
 }
