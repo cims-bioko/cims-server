fa8062a4115fa4aae3ec606692a8d95a5765d12c
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2015-02-03 12:19:02 -0500

    Wire in the old writeResponse method to the LocationResource

--- webservice/src/main/java/org/openhds/webservice/resources/LocationResource.java
+++ webservice/src/main/java/org/openhds/webservice/resources/LocationResource.java
@@ -140,7 +140,7 @@ public class LocationResource {
     @RequestMapping(value = "/cached", method = RequestMethod.GET, produces = "application/xml")
     public void getAllCachedLocationsXml(HttpServletResponse response) {
         try {
-            cacheResponseWriter.refactoredWriteResponse(fileResolver.resolveLocationXmlFilename(), response);
+            cacheResponseWriter.writeResponse(fileResolver.resolveLocationXmlFile(), response);
         } catch (IOException e) {
             logger.error("Problem writing location xml file: " + e.getMessage());
         }
@@ -149,7 +149,7 @@ public class LocationResource {
     @RequestMapping(value = "/cached", method = RequestMethod.GET, produces = "application/json")
     public void getAllCachedLocationsJson(HttpServletResponse response) {
         try {
-            cacheResponseWriter.refactoredWriteResponse(fileResolver.resolveLocationXmlFilename(), response);
+            cacheResponseWriter.writeResponse(fileResolver.resolveLocationXmlFile(), response);
         } catch (IOException e) {
             logger.error("Problem writing location xml file: " + e.getMessage());
         }
