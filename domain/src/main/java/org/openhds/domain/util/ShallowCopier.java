8461f4b205e1a70da5ce0340334f03632d381894
Ben Heasly <benjamin.heasly@gmail.com>
2015-02-25 15:37:34 -0500

    drop in new refelcive implementaiton of ShallowCopier

--- domain/src/main/java/org/openhds/domain/util/ShallowCopier.java
+++ domain/src/main/java/org/openhds/domain/util/ShallowCopier.java
@@ -1,243 +1,203 @@
 package org.openhds.domain.util;
 
-import org.openhds.domain.model.FieldWorker;
-import org.openhds.domain.model.Individual;
-import org.openhds.domain.model.Location;
-import org.openhds.domain.model.LocationHierarchy;
-import org.openhds.domain.model.Membership;
-import org.openhds.domain.model.PregnancyOutcome;
-import org.openhds.domain.model.Relationship;
-import org.openhds.domain.model.Residency;
-import org.openhds.domain.model.SocialGroup;
-import org.openhds.domain.model.Visit;
+
+import org.openhds.domain.model.UuidIdentifiable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Use reflection to make a shallow copy of a UuidIdentifiable object.
+ *
+ * This ShallowCopier uses reflection to build a field-by-field copy of a given
+ * UuidIdentifiable.  When any field refers to another UuidIdentifiable object
+ * (directly or through a Collection), the copy will point to a "stub" object instead
+ * of the original object.  The "stub" is a default instance of the same class with
+ * only its uuid and no other fields set.
+ *
+ * Stubs give external clients enough information to rebuild object references.  Since
+ * the stubs don't refer to any other objects, they prevent runaway recursion by tools
+ * like automatic XML Marshallers.
+ *
+ * This shallow copier assumes that all objects being copied or converted to stubs have
+ * no-argument constructors that produce a "default" or "blank" object.
+ *
+ * "Shallow copy" has special meaning for OpenHDS because shallow copies are what we
+ * send over the wire to external clients, like the OpenHDS tablet.  This is different
+ * from the meaning of "shallow copy" in the context of the Java Cloneable interface.
+ *
+ * BSH
+ */
 public class ShallowCopier {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(ShallowCopier.class);
+    private static final Logger logger = LoggerFactory.getLogger(ShallowCopier.class);
 
-
-
-    public static FieldWorker shallowCopyFieldWorker(FieldWorker fieldWorker) {
-        FieldWorker copy = new FieldWorker();
-        copy.setUuid(fieldWorker.getUuid());
-        copy.setIdPrefix(fieldWorker.getIdPrefix());
-        copy.setExtId(fieldWorker.getExtId());
-        copy.setFirstName(fieldWorker.getFirstName());
-        copy.setLastName(fieldWorker.getLastName());
-        copy.setPasswordHash(fieldWorker.getPasswordHash());
+    // Make a shallow copy of the given object.
+    public UuidIdentifiable makeShallowCopy(UuidIdentifiable original) {
+        UuidIdentifiable copy = getDefaultBlank(original);
+        Set<Field> allFields = getAllFields(original);
+        assignAllFields(original, copy, allFields);
         return copy;
     }
 
-    public static Residency shallowCopyResidency(Residency residency) {
-        Residency copy = new Residency();
-        copy.setUuid(residency.getUuid());
-        copy.setCollectedBy(residency.getCollectedBy());
-        FieldWorker fieldWorkerStub = FieldWorker.makeStub(residency.getCollectedBy().getUuid(), residency.getCollectedBy().getExtId());
-        copy.setCollectedBy(fieldWorkerStub);
-        Individual individualStub = Individual.makeStub(residency.getIndividual().getUuid(), residency.getIndividual().getExtId());
-        copy.setIndividual(individualStub);
-        Location locationStub = Location.makeStub(residency.getLocation().getUuid(), residency.getLocation().getExtId());
-        copy.setLocation(locationStub);
-        copy.setStartDate(residency.getStartDate());
-        copy.setStartType(residency.getStartType());
-        copy.setEndDate(residency.getEndDate());
-        copy.setEndType(residency.getEndType());
-        return copy;
+    // Walk up the inheritance hierarchy for the given object.
+    private Set<Class<?>> getInheritanceHierarchy(UuidIdentifiable original) {
+        Set<Class<?>> superclasses = new HashSet<Class<?>>();
+        Class<?> currentClass = original.getClass();
+        while (!currentClass.equals(Object.class)) {
+            superclasses.add(currentClass);
+            currentClass = currentClass.getSuperclass();
+        }
+        return superclasses;
     }
 
-    public static Individual shallowCopyIndividual(Individual individual) {
+    // Get all declared fields of the given object and its supertypes.
+    private Set<Field> getAllFields(UuidIdentifiable original) {
+        Set<Field> allFields = new HashSet<Field>();
+        Set<Class<?>> superclasses = getInheritanceHierarchy(original);
+        for (Class<?> clazz : superclasses) {
+            allFields.addAll(getDeclaredFields(clazz));
+        }
+        return allFields;
+    }
 
-        Individual copy = new Individual();
+    // Get all declared fields for the given class.
+    private Set<Field> getDeclaredFields(Class<?> clazz) {
+        Set<Field> declaredFields = new HashSet<Field>();
+        declaredFields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+        return declaredFields;
+    }
+
+    // Make a new blank object of the same concrete class as the given object using its no-argument constructor.
+    private UuidIdentifiable getDefaultBlank(UuidIdentifiable original) {
+        Class<UuidIdentifiable> currentClass = (Class<UuidIdentifiable>) original.getClass();
+        Constructor<UuidIdentifiable> constructor;
         try {
-            copy.setUuid(individual.getUuid());
-            copy.setDob(individual.getDob());
-            copy.setExtId(individual.getExtId());
-            copy.setFather(copyIds(individual.getFather()));
-            copy.setFirstName(individual.getFirstName());
-            copy.setGender(individual.getGender());
-            copy.setLastName(individual.getLastName());
-            String middleName = individual.getMiddleName() == null ? ""
-                    : individual.getMiddleName();
-            copy.setMiddleName(middleName);
-            copy.setMother(copyIds(individual.getMother()));
+            constructor = currentClass.getConstructor();
+        } catch (NoSuchMethodException e) {
+            logger.error("Can't find constructor NoSuchMethodException: " + e.getMessage());
+            return null;
+        }
 
-            copy.setAge(individual.getAge());
-            copy.setAgeUnits(individual.getAgeUnits());
-            copy.setPhoneNumber(individual.getPhoneNumber());
-            copy.setOtherPhoneNumber(individual.getOtherPhoneNumber());
-            copy.setLanguagePreference(individual.getLanguagePreference());
-            copy.setPointOfContactName(individual.getPointOfContactName());
-            copy.setPointOfContactPhoneNumber(individual
-                    .getPointOfContactPhoneNumber());
-            copy.setDip(individual.getDip());
-            copy.setMemberStatus(individual.getMemberStatus());
-            copy.setNationality(individual.getNationality());
+        UuidIdentifiable blank = null;
+        try {
+            blank = constructor.newInstance();
+        } catch (InstantiationException e) {
+            logger.error("Can't invoke constructor InstantiationException: " + e.getMessage());
+        } catch (IllegalAccessException e) {
+            logger.error("Can't invoke constructor IllegalAccessException: " + e.getMessage());
+        } catch (InvocationTargetException e) {
+            logger.error("Can't invoke constructor InvocationTargetException: " + e.getMessage());
+        }
 
-            Individual individualStub = Individual.makeStub(individual.getUuid(), individual.getExtId());
+        return blank;
+    }
 
-            for (Membership membership : individual.getAllMemberships()) {
-                SocialGroup socialGroupStub = SocialGroup.makeStub(membership
-                        .getSocialGroup().getUuid(), membership.getSocialGroup().getExtId());
-                Membership membershipStub = Membership.makeStub(membership.getUuid(), socialGroupStub, individualStub, membership.getbIsToA());
-                copy.getAllMemberships().add(membershipStub);
+    // Make a new stub object of the same concrete class as the given object with only its uuid set.
+    private UuidIdentifiable makeStub(UuidIdentifiable original) {
+        UuidIdentifiable stub = (UuidIdentifiable) getDefaultBlank(original);
+        stub.setUuid(original.getUuid());
+        return stub;
+    }
+
+    // Copy multiple fields from an original object to a target of a compatible class.  Make stubs as necessary.
+    private void assignAllFields(UuidIdentifiable original, UuidIdentifiable target, Set<Field> fields) {
+        for (Field field : fields) {
+            // direct reference to UuidIdentifiable
+            if (UuidIdentifiable.class.isAssignableFrom(field.getType())) {
+                assignStub(original, target, field);
+                continue;
             }
 
-            Residency currentResidency = individual.getCurrentResidency();
-            if (null != currentResidency) {
-                Location locationStub = Location.makeStub(currentResidency.getLocation().getUuid(), currentResidency.getLocation().getExtId());
-                locationStub.setLocationHierarchy(null);
-                Residency residencyStub = Residency.makeStub(currentResidency.getUuid(), locationStub,individualStub);
-                residencyStub.setEndType(currentResidency.getEndType());
-                copy.getAllResidencies().add(residencyStub);
+            // Collection may contain UuidIdentifiables
+            if (Collection.class.isAssignableFrom(field.getType())) {
+                addStubsToCollection(original, target, field);
+                continue;
             }
-        } catch (Exception e) {
-            System.out.println(copy.getExtId());
+
+            // default simple assignment
+            assignField(original, target, field);
         }
-        return copy;
     }
 
-    private static Individual copyIds(Individual individual) {
-        Individual copy = new Individual();
-        if (individual == null) {
-            logger.warn("Individual had a null father or mother - using UNK as default value");
-            copy.setExtId("UNK");
-            copy.setUuid("UNK-UUID");
-        } else {
-            copy.setExtId(individual.getExtId());
-            copy.setUuid(individual.getUuid());
+    // Copy the given field verbatim from an original object to a target of a compatible class.
+    private void assignField(UuidIdentifiable original, UuidIdentifiable target, Field field) {
+        if (null == field) {
+            return;
         }
 
-        return copy;
+        try {
+            field.setAccessible(true);
+            field.set(target, field.get(original));
+        } catch (IllegalAccessException e) {
+            logger.error("Can't assign field <" + field.getName() + "> IllegalAccessException: " + e.getMessage());
+        }
     }
 
-    public static Location shallowCopyLocation(Location loc) {
-        Location copy = new Location();
-
-        copy.setUuid(loc.getUuid());
-        copy.setExtId(loc.getExtId());
-        copy.setAccuracy(getEmptyStringIfBlank(loc.getAccuracy()));
-        copy.setAltitude(getEmptyStringIfBlank(loc.getAltitude()));
-        copy.setLatitude(getEmptyStringIfBlank(loc.getLatitude()));
-        copy.setLongitude(getEmptyStringIfBlank(loc.getLongitude()));
-
-        // extensions for bioko island project
-        copy.setRegionName(getEmptyStringIfBlank(loc.getRegionName()));
-        copy.setProvinceName(getEmptyStringIfBlank(loc.getProvinceName()));
-        copy.setDistrictName(getEmptyStringIfBlank(loc.getDistrictName()));
-        copy.setSubDistrictName(getEmptyStringIfBlank(loc.getSubDistrictName()));
-        copy.setSectorName(getEmptyStringIfBlank(loc.getSectorName()));
-        copy.setLocalityName(getEmptyStringIfBlank(loc.getLocalityName()));
-        copy.setCommunityName(getEmptyStringIfBlank(loc.getCommunityName()));
-        copy.setCommunityCode(getEmptyStringIfBlank(loc.getCommunityCode()));
-        copy.setMapAreaName(getEmptyStringIfBlank(loc.getMapAreaName()));
-
-        copy.setBuildingNumber(getEmptyStringIfBlank(loc.getBuildingNumber()));
-        copy.setFloorNumber(getEmptyStringIfBlank(loc.getFloorNumber()));
-        copy.setProvinceName(getEmptyStringIfBlank(loc.getProvinceName()));
-        copy.setRegionName(getEmptyStringIfBlank(loc.getRegionName()));
-        copy.setSubDistrictName(getEmptyStringIfBlank(loc.getSubDistrictName()));
-        copy.setDistrictName(getEmptyStringIfBlank(loc.getDistrictName()));
-
-        copy.setDescription(getEmptyStringIfBlank(loc.getDescription()));
-
-        LocationHierarchy hierarchy = new LocationHierarchy();
-        hierarchy.setUuid(loc.getLocationHierarchy().getUuid());
-        hierarchy.setExtId(loc.getLocationHierarchy().getExtId());
-        copy.setLocationHierarchy(hierarchy);
-
-        copy.setLocationName(loc.getLocationName());
-        copy.setLocationType(loc.getLocationType());
-
-        FieldWorker fieldworkerStub = FieldWorker.makeStub(loc.getCollectedBy().getUuid(), loc.getCollectedBy().getExtId());
-        copy.setCollectedBy(fieldworkerStub);
-        return copy;
-    }
-
-    private static String getEmptyStringIfBlank(String accuracy) {
-        if (accuracy == null || accuracy.trim().isEmpty()) {
-            return "";
+    // Make a stub based on original an object's UuidIdentifiable field and assign it to a target of a compatible class.
+    private void assignStub(UuidIdentifiable original, UuidIdentifiable target, Field field) {
+        if (null == field) {
+            return;
         }
 
-        return accuracy;
+        if (!UuidIdentifiable.class.isAssignableFrom(field.getType())) {
+            logger.error("Can't assign <"
+                    + field.getType()
+                    + "> to UuidIdentifiable field named <"
+                    + field.getName()
+                    + ">");
+            return;
+        }
+
+        try {
+            field.setAccessible(true);
+            UuidIdentifiable originalEntity = (UuidIdentifiable) field.get(original);
+            UuidIdentifiable stub = makeStub(originalEntity);
+            field.set(target, stub);
+        } catch (IllegalAccessException e) {
+            logger.error("Can't assign UuidIdentifiable stub to field <"
+                    + field.getName()
+                    + "> IllegalAccessException: "
+                    + e.getMessage());
+        }
     }
 
-    public static Relationship shallowCopyRelationship(Relationship original) {
-        Relationship copy = new Relationship();
-        copy.setUuid(original.getUuid());
-        copy.setaIsToB(original.getaIsToB());
+    // Copy elements from an original object's Collection to a compatible target's Collection.  Make stubs as necessary.
+    private void addStubsToCollection(UuidIdentifiable original, UuidIdentifiable target, Field field) {
+        if (null == field) {
+            return;
+        }
 
-        Individual individualStub = Individual.makeStub(original.getIndividualA().getUuid(), original.getIndividualA().getExtId());
-        copy.setIndividualA(individualStub);
+        if (!Collection.class.isAssignableFrom(field.getType())) {
+            logger.error("Can't assign <" + field.getType() + "> to Collection field named <" + field.getName() + ">");
+            return;
+        }
 
-        individualStub = Individual.makeStub(original.getIndividualB().getUuid(), original.getIndividualB().getExtId());
-        copy.setIndividualB(individualStub);
+        try {
+            field.setAccessible(true);
+            Collection<?> originalCollection = (Collection<?>) field.get(original);
+            Collection<Object> stubCollection = (Collection<Object>) field.get(target);
+            for (Object object : originalCollection) {
+                if (UuidIdentifiable.class.isAssignableFrom(object.getClass())) {
+                    UuidIdentifiable stub = makeStub((UuidIdentifiable) object);
+                    stubCollection.add(stub);
+                } else {
+                    stubCollection.add(object);
+                }
+            }
 
-        copy.setStartDate(original.getStartDate());
-        copy.setEndDate(original.getEndDate());
-        copy.setEndType(original.getEndType());
-        return copy;
-    }
-
-    public static SocialGroup shallowCopySocialGroup(SocialGroup original) {
-        Individual groupHeadStub = Individual.makeStub(original.getGroupHead().getUuid(), original.getGroupHead().getExtId());
-        Location locationStub = Location.makeStub(original.getLocation().getUuid(), original.getLocation().getExtId());
-
-        SocialGroup copy = new SocialGroup();
-        copy.setUuid(original.getUuid());
-        copy.setExtId(original.getExtId());
-        copy.setGroupHead(groupHeadStub);
-        copy.setGroupName(original.getGroupName());
-        copy.setLocation(locationStub);
-        return copy;
-    }
-
-    public static Visit shallowCopyVisit(Visit original) {
-        FieldWorker fieldworkerStub = FieldWorker.makeStub(original.getCollectedBy().getUuid(), original.getCollectedBy().getExtId());
-        Location locationStub = Location.makeStub(original.getVisitLocation().getUuid(), original.getVisitLocation().getExtId());
-        locationStub.setLocationHierarchy(null);
-
-        Visit copy = new Visit();
-        copy.setUuid(original.getUuid());
-        copy.setCollectedBy(fieldworkerStub);
-        copy.setVisitLocation(locationStub);
-        copy.setExtId(original.getExtId());
-        copy.setRoundNumber(original.getRoundNumber());
-        copy.setVisitDate(original.getVisitDate());
-
-        return copy;
-    }
-
-    public static Membership shallowCopyMembership(Membership membership) {
-    	
-        Membership copy = new Membership();
-        copy.setUuid(membership.getUuid());
-        copy.setbIsToA(membership.getbIsToA());
-        copy.setStartDate(membership.getStartDate());
-        copy.setStartType(membership.getStartType());
-        copy.setEndDate(membership.getEndDate());
-        copy.setEndType(membership.getEndType());
-        FieldWorker fieldworkerStub = FieldWorker.makeStub(membership.getCollectedBy().getUuid(), membership.getCollectedBy().getExtId());
-        copy.setCollectedBy(fieldworkerStub);
-        Individual individualStub = Individual.makeStub(membership.getIndividual().getUuid(), membership.getIndividual().getExtId());
-        copy.setIndividual(individualStub);
-        SocialGroup sgStub = SocialGroup.makeStub(membership.getSocialGroup().getUuid(), membership.getSocialGroup().getExtId());
-        copy.setSocialGroup(sgStub);
-        return copy;
-    }
-
-    public static PregnancyOutcome shallowCopyPregnancyOutcome(PregnancyOutcome outcome) {
-        PregnancyOutcome copy = new PregnancyOutcome();
-        copy.setUuid(outcome.getUuid());
-        copy.setOutcomeDate(outcome.getOutcomeDate());
-        copy.setChildEverBorn(outcome.getChildEverBorn());
-        copy.setNumberOfLiveBirths(outcome.getNumberOfLiveBirths());
-        copy.setFather(Individual.makeStub(outcome.getFather().getUuid(), outcome.getFather().getExtId()));
-        copy.setMother(Individual.makeStub(outcome.getMother().getUuid(), outcome.getMother().getExtId()));
-        copy.setVisit(Visit.makeStub(outcome.getVisit().getUuid()));
-        return copy;
+        } catch (IllegalAccessException e) {
+            logger.error("Can't add element to Collection field <"
+                    + field.getName()
+                    + "> IllegalAccessException: "
+                    + e.getMessage());
+        }
     }
 }
