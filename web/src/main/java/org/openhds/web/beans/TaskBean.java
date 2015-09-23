d04bfc237fe30b6a17c6523ec23e77c904e229e1
Brent Atkinson <brent.atkinson@gmail.com>
2015-09-23 19:40:38 -0400

    Added ability to schedule cache file building.

--- web/src/main/java/org/openhds/web/beans/TaskBean.java
+++ web/src/main/java/org/openhds/web/beans/TaskBean.java
@@ -1,22 +1,31 @@
 package org.openhds.web.beans;
 
 import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.openhds.domain.model.AsyncTask;
 import org.openhds.task.service.AsyncTaskService;
 import org.openhds.task.support.TaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.support.CronTrigger;
 
 public class TaskBean {
 
     private static final String TASK_VIEW = "task";
     private TaskExecutor taskExecutor;
     private AsyncTaskService asyncTaskService;
+    private TaskScheduler scheduler;
+    private ScheduledFuture scheduledTask;
     
     private Integer roundNumber;
+    private String cronSchedule = "0 0 * * * ?";
 
-    public TaskBean(TaskExecutor taskExecutor, AsyncTaskService asyncTaskService) {
+    public TaskBean(TaskExecutor taskExecutor, AsyncTaskService asyncTaskService, TaskScheduler scheduler) {
         this.taskExecutor = taskExecutor;
         this.asyncTaskService = asyncTaskService;
+        this.scheduler = scheduler;
     }
 
     public String startIndividualTask() {
@@ -55,6 +64,24 @@ public class TaskBean {
     }
 
     public String startAllTasks() {
+        startTasks();
+        return TASK_VIEW;
+    }
+
+    class StartTasksExecutor implements Runnable {
+        @Override
+        public void run() {
+            TaskBean.this.startTasks();
+        }
+    }
+
+    public String scheduleAllTasks() {
+        cancelScheduled();
+        scheduledTask = scheduler.schedule(new StartTasksExecutor(), new CronTrigger(cronSchedule));
+        return TASK_VIEW;
+    }
+
+    private void startTasks() {
         startIndividualTask();
         startLocationTask();
         startRelationshipTask();
@@ -63,6 +90,24 @@ public class TaskBean {
         startMembershipTask();
         startVisitTask();
         startLocationTask();
+    }
+
+    public String getNextScheduledRun() {
+        if (scheduledTask == null) {
+            return "None";
+        }
+        return "In " + scheduledTask.getDelay(TimeUnit.MINUTES) + " min";
+    }
+
+    private void cancelScheduled() {
+        if (scheduledTask != null && !scheduledTask.isCancelled()) {
+            scheduledTask.cancel(false);
+            scheduledTask = null;
+        }
+    }
+
+    public String cancelScheduledTasks() {
+        cancelScheduled();
         return TASK_VIEW;
     }
 
@@ -76,5 +121,13 @@ public class TaskBean {
 
     public void setRoundNumber(Integer roundNumber) {
         this.roundNumber = roundNumber;
+    }
+
+    public String getCronSchedule() {
+        return cronSchedule;
+    }
+
+    public void setCronSchedule(String cronSchedule) {
+        this.cronSchedule = cronSchedule;
     }
 }
