020981b6b596af9eb22e8183dc870bb4ed2353ae
Brent Atkinson <brent.atkinson@gmail.com>
2015-11-14 13:34:56 -0500

    Fixes #96: Certain transitions from webflow links return 404

--- web/src/main/java/org/openhds/web/ui/FacesNavigation.java
+++ web/src/main/java/org/openhds/web/ui/FacesNavigation.java
@@ -1,24 +1,62 @@
 package org.openhds.web.ui;
 
-public class FacesNavigation {
-	
-	private String navigateTo;
+import com.sun.faces.application.ApplicationAssociate;
+import com.sun.faces.application.ConfigNavigationCase;
 
-	public boolean setNavigateTo(String navigateTo) {
-		this.navigateTo = navigateTo;
-		
-		return true;
-	}
+import org.springframework.web.context.ServletContextAware;
 
-	public String getNavigateTo() {
-		// need to check if user clicked on a regular jsf amendment form
-		// or a web flow
-		if (navigateTo.indexOf("flow") > 0) {
-			// user clicked a flow link - just need to replace the _ with a .
-			return navigateTo.replace('_', '.');
-		} else {
-			// user clicked a regular amendment form link
-			return navigateTo.replace('_', '/') + ".faces";
-		}
-	}	
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+/**
+ * A utility for determining the view for a given outcome based on JSF config.
+ */
+public class FacesNavigation implements ServletContextAware {
+
+    public static final String GLOBAL = "*";
+
+    private String eventId;
+    private ServletContext ctx;
+    private Map<String, String> viewMap;
+
+    public boolean setNavigateTo(String eventId) {
+        this.eventId = eventId;
+        return true;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        ctx = servletContext;
+    }
+
+    /**
+     * Builds a mapping from outcome to view id for all navigation cases that apply globally.
+     */
+    private Map<String, String> buildViewMap(ApplicationAssociate appAssoc) {
+        Map<String, String> result = new HashMap<>();
+        for (ConfigNavigationCase navCase : appAssoc.getNavigationCaseListMappings().get(GLOBAL)) {
+            String outcome = navCase.getFromOutcome();
+            if (outcome != null) {
+                result.put(outcome, navCase.getToViewId());
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Lazy-loads a singleton map containing outcome-to-view mappings. It should only be called once JSF has started.
+     */
+    private Map<String, String> getViewMap() {
+        if (viewMap == null) {
+            viewMap = buildViewMap(ApplicationAssociate.getInstance(ctx));
+        }
+        return viewMap;
+    }
+
+    public String getNavigateTo() {
+        Map<String, String> views = getViewMap();
+        return views != null && views.containsKey(eventId) ? views.get(eventId) : eventId;
+    }
 }
