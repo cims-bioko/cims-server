95de5d3c6b0242241ca238165d6ae1e95eb19522
Brent Atkinson <brent.atkinson@gmail.com>
2015-11-22 21:22:46 -0500

    #108: Updated navigation utility for JSF 2 api changes.

--- web/src/main/java/org/openhds/web/ui/FacesNavigation.java
+++ web/src/main/java/org/openhds/web/ui/FacesNavigation.java
@@ -1,62 +1,27 @@
 package org.openhds.web.ui;
 
-import com.sun.faces.application.ApplicationAssociate;
-import com.sun.faces.application.ConfigNavigationCase;
-
-import org.springframework.web.context.ServletContextAware;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.ServletContext;
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.application.NavigationCase;
+import javax.faces.context.FacesContext;
 
 /**
  * A utility for determining the view for a given outcome based on JSF config.
  */
-public class FacesNavigation implements ServletContextAware {
+public class FacesNavigation {
 
     public static final String GLOBAL = "*";
 
     private String eventId;
-    private ServletContext ctx;
-    private Map<String, String> viewMap;
 
     public boolean setNavigateTo(String eventId) {
         this.eventId = eventId;
         return true;
     }
 
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-        ctx = servletContext;
-    }
-
-    /**
-     * Builds a mapping from outcome to view id for all navigation cases that apply globally.
-     */
-    private Map<String, String> buildViewMap(ApplicationAssociate appAssoc) {
-        Map<String, String> result = new HashMap<>();
-        for (ConfigNavigationCase navCase : appAssoc.getNavigationCaseListMappings().get(GLOBAL)) {
-            String outcome = navCase.getFromOutcome();
-            if (outcome != null) {
-                result.put(outcome, navCase.getToViewId());
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Lazy-loads a singleton map containing outcome-to-view mappings. It should only be called once JSF has started.
-     */
-    private Map<String, String> getViewMap() {
-        if (viewMap == null) {
-            viewMap = buildViewMap(ApplicationAssociate.getInstance(ctx));
-        }
-        return viewMap;
-    }
-
     public String getNavigateTo() {
-        Map<String, String> views = getViewMap();
-        return views != null && views.containsKey(eventId) ? views.get(eventId) : eventId;
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ConfigurableNavigationHandler handler = (ConfigurableNavigationHandler) ctx.getApplication().getNavigationHandler();
+        NavigationCase navCase = handler.getNavigationCase(ctx, null, eventId);
+        return navCase != null ? navCase.getToViewId(ctx) : eventId;
     }
 }
