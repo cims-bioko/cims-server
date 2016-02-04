adceb85eb3f16ceacc4675e81bb03ebe9b3eef4a
Brent Atkinson <brent.atkinson@gmail.com>
2016-02-04 11:10:30 -0500

    Decoupled visit cache file building from ORM mappings.

--- task/src/main/java/org/openhds/task/VisitXmlWriterTask.java
+++ task/src/main/java/org/openhds/task/VisitXmlWriterTask.java
@@ -1,16 +1,30 @@
 package org.openhds.task;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.openhds.domain.model.Visit;
+import org.hibernate.SharedSessionContract;
+import org.hibernate.type.CalendarType;
+import org.openhds.domain.util.CalendarAdapter;
 import org.openhds.task.service.AsyncTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.stream.XMLStreamWriter;
+
+import static org.hibernate.transform.Transformers.aliasToBean;
+
 @Component("visitXmlWriter")
-public class VisitXmlWriterTask extends XmlWriterTemplate<Visit> {
+public class VisitXmlWriterTask extends XmlWriterTemplate<VisitXmlWriterTask.VisitXml> {
 
     @Autowired
     public VisitXmlWriterTask(AsyncTaskService asyncTaskService, SessionFactory factory) {
@@ -18,10 +32,19 @@ public class VisitXmlWriterTask extends XmlWriterTemplate<Visit> {
     }
 
     @Override
+    protected Query createQuery(SharedSessionContract session, String query) {
+        return session.createSQLQuery(query)
+                .addScalar("uuid")
+                .addScalar("extId")
+                .addScalar("location")
+                .addScalar("visitDate", CalendarType.INSTANCE)
+                .addScalar("collectedBy")
+                .setResultTransformer(aliasToBean(VisitXml.class));
+    }
+
+    @Override
     protected String getExportQuery() {
-        return "from Visit" +
-                " where deleted = false" +
-                " and roundNumber = :round";
+        return "select * from v_visit_sync where round = :round";
     }
 
     @Override
@@ -36,13 +59,30 @@ public class VisitXmlWriterTask extends XmlWriterTemplate<Visit> {
     }
 
     @Override
+    protected void writeXml(XMLStreamWriter xmlStreamWriter, Marshaller marshaller, VisitXml original)
+            throws JAXBException {
+        marshaller.marshal(original, xmlStreamWriter);
+    }
+
+    @Override
     protected Class<?> getBoundClass() {
-        return Visit.class;
+        return VisitXml.class;
     }
 
     @Override
     protected String getStartElementName() {
         return "visits";
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlRootElement(name = "visit")
+    public static class VisitXml {
+        public String uuid;
+        public String extId;
+        public String location;
+        @XmlJavaTypeAdapter(CalendarAdapter.class)
+        public Calendar visitDate;
+        public String collectedBy;
     }
 
 }
