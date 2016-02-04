bf40576523c1f628352b26193cc4c1a96b39123f
Brent Atkinson <brent.atkinson@gmail.com>
2016-02-04 11:00:53 -0500

    Decoupled socialgroup cache file building from ORM mappings.

--- task/src/main/java/org/openhds/task/SocialGroupXmlWriterTask.java
+++ task/src/main/java/org/openhds/task/SocialGroupXmlWriterTask.java
@@ -1,13 +1,23 @@
 package org.openhds.task;
 
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.openhds.domain.model.SocialGroup;
+import org.hibernate.SharedSessionContract;
 import org.openhds.task.service.AsyncTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.stream.XMLStreamWriter;
+
+import static org.hibernate.transform.Transformers.aliasToBean;
+
 @Component("socialGroupXmlWriter")
-public class SocialGroupXmlWriterTask extends XmlWriterTemplate<SocialGroup> {
+public class SocialGroupXmlWriterTask extends XmlWriterTemplate<SocialGroupXmlWriterTask.SocialGroupXml> {
 
     @Autowired
     public SocialGroupXmlWriterTask(AsyncTaskService asyncTaskService, SessionFactory factory) {
@@ -15,19 +25,43 @@ public class SocialGroupXmlWriterTask extends XmlWriterTemplate<SocialGroup> {
     }
 
     @Override
+    protected Query createQuery(SharedSessionContract session, String query) {
+        return session.createSQLQuery(query)
+                .addScalar("uuid")
+                .addScalar("head")
+                .addScalar("location")
+                .addScalar("name")
+                .setResultTransformer(aliasToBean(SocialGroupXml.class));
+    }
+
+    @Override
     protected String getExportQuery() {
-        return "from SocialGroup" +
-                " where deleted = false";
+        return "select * from v_socialgroup_sync";
+    }
+
+    @Override
+    protected void writeXml(XMLStreamWriter xmlStreamWriter, Marshaller marshaller, SocialGroupXml original)
+            throws JAXBException {
+        marshaller.marshal(original, xmlStreamWriter);
     }
 
     @Override
     protected Class<?> getBoundClass() {
-        return SocialGroup.class;
+        return SocialGroupXml.class;
     }
 
     @Override
     protected String getStartElementName() {
         return "socialgroups";
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlRootElement(name = "socialgroup")
+    public static class SocialGroupXml {
+        public String uuid;
+        public String head;
+        public String location;
+        public String name;
     }
 
 }
