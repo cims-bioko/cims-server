74ec9e1e4931c350f6ef7780f96a353f0096f61f
Brent Atkinson <brent.atkinson@gmail.com>
2016-02-07 13:17:14 -0500

    Trimming down to bare essentials, removing angular.

--- web/src/main/webapp/resources/scripts/ng/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('tabletuing.services', ['ngResource']).
-  value('version', '0.1');
