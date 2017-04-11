9f1889c03067d27c1a7a84daf48360b8793d924b
Brent Atkinson <brent.atkinson@gmail.com>
2017-04-11 12:55:41 +0100

    Removed unused constants.

--- src/main/java/com/github/cimsbioko/server/webapi/Headers.java
+++ src/main/java/com/github/cimsbioko/server/webapi/Headers.java
@@ -1,8 +1,5 @@
 package com.github.cimsbioko.server.webapi;
 
 public class Headers {
-    public static final String IF_NONE_MATCH = "If-None-Match";
     public static final String ACCEPT = "Accept";
-    public static final String ETAG = "ETag";
-    public static final String CONTENT_TYPE = "Content-Type";
 }
