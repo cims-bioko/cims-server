a0103975addf6c162c95dc576a5e919ec12e8f5f
Brent Atkinson <brent.atkinson@gmail.com>
2017-04-11 12:55:32 +0100

    Removed unused import.

--- src/main/java/com/github/cimsbioko/server/webapi/AbstractFormResource.java
+++ src/main/java/com/github/cimsbioko/server/webapi/AbstractFormResource.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.xml.bind.Marshaller;
 import java.util.Calendar;
 
 public class AbstractFormResource {
