8146cdce7dc8f9c719ef906c0594fe1589d60d26
Brent Atkinson <brent.atkinson@gmail.com>
2017-04-21 16:04:39 +0100

    Removed unused import.

--- src/main/java/com/github/cimsbioko/server/webapi/ODKFormsResource.java
+++ src/main/java/com/github/cimsbioko/server/webapi/ODKFormsResource.java
@@ -2,7 +2,6 @@ package com.github.cimsbioko.server.webapi;
 
 import com.github.cimsbioko.server.controller.exception.ExistingSubmissionException;
 import com.github.cimsbioko.server.controller.service.FormSubmissionService;
-import com.github.cimsbioko.server.dao.FormSubmissionDao;
 import com.github.cimsbioko.server.domain.model.FormSubmission;
 import org.jdom2.Document;
 import org.jdom2.Element;
