6ffd49ffff3053b526c64b7b1d5c7c2112127e3f
Brent Atkinson <brent.atkinson@gmail.com>
2017-08-01 11:04:31 +0100

    Ported #138 from 2.X: Add ability to download mobile database as user

--- src/main/java/com/github/cimsbioko/server/webapi/MobileDatabaseResource.java
+++ src/main/java/com/github/cimsbioko/server/webapi/MobileDatabaseResource.java
@@ -4,14 +4,19 @@ import com.github.batkinson.jrsync.Metadata;
 import com.github.cimsbioko.server.task.service.AsyncTaskService;
 import com.github.cimsbioko.server.task.support.FileResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import static com.github.cimsbioko.server.Application.WebConfig.CACHED_FILES_PATH;
 import static com.github.cimsbioko.server.task.service.AsyncTaskService.MOBILEDB_TASK_NAME;
@@ -30,6 +35,8 @@ public class MobileDatabaseResource {
 
     public static final String MOBILEDB_PATH = "/mobiledb/cached";
     public static final String SQLITE_MIME_TYPE = "application/x-sqlite3";
+    public static final String MOBILEDB_EXPORT_PATH = "/mobiledb/export";
+    public static final String INSTALLABLE_FILENAME = "openhds.db";
 
     @Autowired
     private FileResolver fileResolver;
@@ -53,5 +60,29 @@ public class MobileDatabaseResource {
             return "forward:" + CACHED_FILES_PATH + "/" + metadataFile.getName();
         }
         return "forward:" + CACHED_FILES_PATH + "/" + cacheFile.getName();
+    }
+
+    @RequestMapping(value = MOBILEDB_EXPORT_PATH, method = RequestMethod.GET)
+    public void browserExport(HttpServletResponse response) throws IOException {
+
+        File dbFile = fileResolver.resolveMobileDBFile();
+        FileSystemResource dbFileRes = new FileSystemResource(dbFile);
+
+        if (!dbFileRes.isReadable()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND,
+                    "Unable to find mobiledb file. Try generating it from the tasks menu.");
+        } else {
+            response.setContentType("application/zip");
+            response.setHeader("Content-Disposition", "attachment; filename=" + INSTALLABLE_FILENAME + ".zip");
+            try (ZipOutputStream zOut = new ZipOutputStream(response.getOutputStream())) {
+                ZipEntry e = new ZipEntry(INSTALLABLE_FILENAME);
+                e.setSize(dbFileRes.contentLength());
+                e.setTime(System.currentTimeMillis());
+                zOut.putNextEntry(e);
+                StreamUtils.copy(dbFileRes.getInputStream(), zOut);
+                zOut.closeEntry();
+                zOut.finish();
+            }
+        }
     }
 }
