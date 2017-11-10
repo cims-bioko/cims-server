f6d677a5beefe8bf4e0d0ae1cdf39b404fbc3b66
Brent Atkinson <brent.atkinson@gmail.com>
2017-11-10 18:31:13 +0100

    Removed unused import

--- src/main/java/com/github/cimsbioko/server/domain/model/User.java
+++ src/main/java/com/github/cimsbioko/server/domain/model/User.java
@@ -13,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.HashSet;
