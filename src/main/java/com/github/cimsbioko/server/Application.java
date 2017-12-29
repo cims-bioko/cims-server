24ca1cadbe62fab75b713ebc6ad6518bb02cdd64
Brent Atkinson <brent.atkinson@gmail.com>
2017-12-29 12:08:11 +0100

    Factored app version config out of app class

--- src/main/java/com/github/cimsbioko/server/Application.java
+++ src/main/java/com/github/cimsbioko/server/Application.java
@@ -1,6 +1,5 @@
 package com.github.cimsbioko.server;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.*;
@@ -8,16 +7,9 @@ import org.springframework.context.annotation.*;
 @Configuration
 @EnableAutoConfiguration
 @ImportResource("classpath:/META-INF/spring/application-context.xml")
-@Import({FileSystemConfig.class, SecurityConfig.class, WebConfig.class})
-@PropertySource(value = "classpath:/META-INF/build-info.properties", ignoreResourceNotFound = true)
+@Import({FileSystemConfig.class, SecurityConfig.class, WebConfig.class, AppVersionConfig.class})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    String appVersion(@Value("${build.version}") String buildVersion) {
-        return buildVersion != null ? buildVersion : "DEV";
     }
 }
