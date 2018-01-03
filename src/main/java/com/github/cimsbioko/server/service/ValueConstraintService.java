dc8b74413b0a7d8a10c7d28b8425bf5d1b237377
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-03 10:37:11 +0100

    Removed unused import

--- src/main/java/com/github/cimsbioko/server/service/ValueConstraintService.java
+++ src/main/java/com/github/cimsbioko/server/service/ValueConstraintService.java
@@ -1,7 +1,5 @@
 package com.github.cimsbioko.server.service;
 
-import org.jdom2.Element;
-
 public interface ValueConstraintService {
 
     boolean isValidConstraintValue(String constraintName, Object value);
