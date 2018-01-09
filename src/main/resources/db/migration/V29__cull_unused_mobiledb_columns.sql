b1a38c36cc52cf4b6ae57a7b04d46bf64163ea9f
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-09 10:31:59 +0100

    Updated generated mobile database to match mobile client

---
+++ src/main/resources/db/migration/V29__cull_unused_mobiledb_columns.sql
@@ -0,0 +1,26 @@
+-- drop columns that the mobile db doesn't need
+drop view v_individual_sync;
+create view v_individual_sync as
+select
+  i.uuid,
+  to_char(i.dob, 'YYYY-MM-DD') AS dob,
+  i.extid,
+  i.first_name AS firstname,
+  i.gender,
+  i.last_name AS lastname,
+  i.home AS currentresidence,
+  i.middle_name AS othernames,
+  i.phone1 AS phonenumber,
+  i.phone2 AS otherphonenumber,
+  i.contact_name AS pointofcontactname,
+  i.contact_phone AS pointofcontactphonenumber,
+  i.language AS languagepreference,
+  'Unknown'::character varying AS memberstatus,
+  i.nationality,
+  i.dip AS otherid
+from
+  individual i
+where
+  not i.deleted
+  and i.extid <> 'UNK'
+  and i.home is not null;
