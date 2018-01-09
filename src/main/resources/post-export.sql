4f3e03f10bb5a9e493811237000c0c8dc8c7123a
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-09 11:47:13 +0100

    Do not generate indexes for already indexed primary keys

--- src/main/resources/post-export.sql
+++ src/main/resources/post-export.sql
@@ -1,11 +1,8 @@
 create index individual_extid_index on individuals(extid)
 create index individual_residency_index on individuals(currentresidence)
 create index location_hierarchy_uuid_index on locations(hierarchyuuid)
-create index location_uuid_index on locations(uuid)
-create index locationhierarchy_uuid_index on hierarchyitems(uuid)
 create index locationhierarchy_extid_index on hierarchyitems(extid)
 create index fieldworkers_extid_index on fieldworkers(extid)
-create index fieldworkers_uuid_index on fieldworkers(uuid)
 create index fieldworkers_id_prefix_index on fieldworkers(idprefix)
 create index fieldworkers_password_index on fieldworkers(password)
 vacuum
