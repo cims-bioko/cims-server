baac3977559f0abc641bebc75cece801c56890bd
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-14 12:04:56 +0100

    Now setting location status on spraying 'destroyed' or 'uninhabited' evals

--- src/main/java/com/github/cimsbioko/server/formproc/forms/SprayingFormProcessor.java
+++ src/main/java/com/github/cimsbioko/server/formproc/forms/SprayingFormProcessor.java
@@ -3,6 +3,8 @@ package com.github.cimsbioko.server.formproc.forms;
 import com.github.cimsbioko.server.domain.Location;
 import com.github.cimsbioko.server.exception.ConstraintViolations;
 import com.github.cimsbioko.server.service.refactor.LocationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +18,31 @@ import java.io.Serializable;
 @Controller
 public class SprayingFormProcessor extends AbstractFormProcessor {
 
+    private static final Logger log = LoggerFactory.getLogger(SprayingFormProcessor.class);
+
     @Autowired
     private LocationService locationService;
 
     @Transactional
     public ResponseEntity<? extends Serializable> processForm(@RequestBody Form form) {
         if (form.entityUuid != null) {
-            try {
-                Location location = locationService.getByUuid(form.entityUuid);
-                switch (form.evaluation) {
-                    case DESTROYED:
-                        location.setDeleted(true);
-                        break;
+            Location location = locationService.getByUuid(form.entityUuid);
+            if (location != null) {
+                try {
+                    switch (form.evaluation) {
+                        case DESTROYED:
+                            location.getAttrsForUpdate().put("status", "destroyed");
+                            location.setDeleted(true);
+                            break;
+                        case UNINHABITED:
+                            location.getAttrsForUpdate().put("status", "uninhabited");
+                    }
+                    locationService.save(location);
+                } catch (ConstraintViolations cv) {
+                    return requestError(cv);
                 }
-                locationService.save(location);
-            } catch (ConstraintViolations cv) {
-                return requestError(cv);
+            } else {
+                log.info("location {} does not exist, ignoring", form.entityUuid);
             }
         }
         return new ResponseEntity<>(form, HttpStatus.CREATED);
