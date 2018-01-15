fc5ca4f7aca38d0c4d7dcbdba62fb7c683b50104
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-15 09:57:58 +0100

    Trimmed site properties to what is actually used

--- src/main/java/com/github/cimsbioko/server/service/SitePropertiesService.java
+++ src/main/java/com/github/cimsbioko/server/service/SitePropertiesService.java
@@ -2,59 +2,6 @@ package com.github.cimsbioko.server.service;
 
 public interface SitePropertiesService {
 
-    String getEarliestEnumerationDate();
-
-    String getSpecialStudyLocation();
-
     String getUnknownIdentifier();
 
-    String getInmigrationCode();
-
-    String getOutmigrationCode();
-
-    String getBirthCode();
-
-    String getDeathCode();
-
-    String getEnumerationCode();
-
-    String getMarriageCode();
-
-    String getNotApplicableCode();
-
-    String getLiveBirthCode();
-
-    String getStillBirthCode();
-
-    String getMiscarriageCode();
-
-    String getAbortionCode();
-
-    String getDataStatusWarningCode();
-
-    String getDataStatusValidCode();
-
-    String getDataStatusFatalCode();
-
-    String getDataStatusVoidCode();
-
-    String getDataStatusPendingCode();
-
-    String getDataStatusClosedCode();
-
-    String getYesResponse();
-
-    String getNoResponse();
-
-    String getMaleCode();
-
-    String getFemaleCode();
-
-    int getMinimumAgeOfParents();
-
-    int getMinimumAgeOfHouseholdHead();
-
-    int getMinimumAgeOfMarriage();
-
-    int getMinimumAgeOfPregnancy();
 }
