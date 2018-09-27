ce39f1b5e4ff9a220787908526011c882cf8f0a2
Brent Atkinson <brent.atkinson@gmail.com>
2018-09-27 17:02:22 +0100

    Fixed jpql-based finder: named params need annotations to bind

--- src/main/java/com/github/cimsbioko/server/dao/FormRepository.java
+++ src/main/java/com/github/cimsbioko/server/dao/FormRepository.java
@@ -5,6 +5,7 @@ import com.github.cimsbioko.server.domain.FormId;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,5 +16,5 @@ public interface FormRepository extends PagingAndSortingRepository<Form, FormId>
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query("update #{#entityName} f set f.downloads = false where f.formId.id = :formId and f.formId.version <> :version")
-    void exclusiveDownload(String formId, String version);
+    void exclusiveDownload(@Param("formId") String formId, @Param("version") String version);
 }
