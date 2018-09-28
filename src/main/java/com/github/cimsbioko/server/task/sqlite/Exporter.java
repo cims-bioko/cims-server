e468ba07b3133064e7a704ad62b5e729c62532a3
Brent Atkinson <brent.atkinson@gmail.com>
2018-09-28 23:13:54 +0100

    Minor code cleanup

--- src/main/java/com/github/cimsbioko/server/task/sqlite/Exporter.java
+++ src/main/java/com/github/cimsbioko/server/task/sqlite/Exporter.java
@@ -18,14 +18,14 @@ public class Exporter {
 
     public void export(String query, String table, File target) throws SQLException, IOException {
         try (Connection src = this.src.createConnection(); Statement s = this.src.createStatement(src);
-             Connection dst = this.dst.createConnection(target, false); Statement d = this.dst.createStatement(dst);
+             Connection dst = this.dst.createConnection(target, false);
              ResultSet rs = s.executeQuery(query)) {
             ResultSetMetaData md = rs.getMetaData();
             Mapper mapper = mapperFactory.createMapper(md, table, dst);
-            try (PreparedStatement di = dst.prepareStatement(mapper.getInsertDml())) {
+            try (PreparedStatement d = dst.prepareStatement(mapper.getInsertDml())) {
                 while (rs.next()) {
-                    mapper.bind(rs, di);
-                    di.executeUpdate();
+                    mapper.bind(rs, d);
+                    d.executeUpdate();
                 }
                 dst.commit();
             }
@@ -35,7 +35,8 @@ public class Exporter {
     public void scriptTarget(InputStream script, File target) throws SQLException, IOException {
         if (script != null) {
             BufferedReader ddlCmds = new BufferedReader(new InputStreamReader(script));
-            try (Connection dst = this.dst.createConnection(target, true); Statement d = this.dst.createStatement(dst)) {
+            try (Connection dst = this.dst.createConnection(target, true);
+                 Statement d = this.dst.createStatement(dst)) {
                 String ddlCmd;
                 while ((ddlCmd = ddlCmds.readLine()) != null && ddlCmd.trim().length() > 0) {
                     d.executeUpdate(ddlCmd);
