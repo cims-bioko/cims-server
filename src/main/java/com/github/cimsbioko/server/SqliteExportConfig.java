43aeee628f3b26ce2c2ec3f7ade0e1fae69a4f3c
Brent Atkinson <brent.atkinson@gmail.com>
2018-09-30 11:10:19 +0100

    Migrated final piece of export config from xml to javaconfig

--- src/main/java/com/github/cimsbioko/server/SqliteExportConfig.java
+++ src/main/java/com/github/cimsbioko/server/SqliteExportConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 public class SqliteExportConfig {
@@ -33,5 +36,12 @@ public class SqliteExportConfig {
     @Bean
     Exporter exporter(Source source) {
         return new Exporter(source, target(), mapperFactory());
+    }
+
+    @Bean
+    public Properties exportQueries(@Value("classpath:/export-queries.properties") InputStream querySource) throws IOException {
+        Properties queries = new Properties();
+        queries.load(querySource);
+        return queries;
     }
 }
