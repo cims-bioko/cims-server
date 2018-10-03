0595dfb9e996f702a822090eaaf374a8fe6f87f1
Brent Atkinson <brent.atkinson@gmail.com>
2018-10-03 23:32:47 +0100

    Reworked forms section: paged listing with modal to add forms

--- src/main/java/com/github/cimsbioko/server/web/FormsController.java
+++ src/main/java/com/github/cimsbioko/server/web/FormsController.java
@@ -1,8 +1,10 @@
 package com.github.cimsbioko.server.web;
 
+import com.github.cimsbioko.server.dao.FormRepository;
 import com.github.cimsbioko.server.service.FormService;
 import org.jdom2.JDOMException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -20,16 +23,18 @@ import static com.github.cimsbioko.server.webapi.odk.Constants.XLSFORM_DEF_FILE;
 @Controller
 public class FormsController {
 
+    private FormRepository repo;
     private FormService service;
 
     @Autowired
-    public FormsController(FormService service) {
+    public FormsController(FormRepository repo, FormService service) {
+        this.repo = repo;
         this.service = service;
     }
 
     @GetMapping("/forms")
-    public String forms() {
-        return "forms";
+    public ModelAndView forms(@RequestParam(name = "p", defaultValue = "0") Integer page) {
+        return new ModelAndView("forms", "forms", repo.findAll(new PageRequest(page, 10)));
     }
 
     @PostMapping("/uploadXmlForm")
