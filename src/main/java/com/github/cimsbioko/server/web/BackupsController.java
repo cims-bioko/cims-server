4472867b04923401fac78787adad1a04e1ecc97d
Brent Atkinson <brent.atkinson@gmail.com>
2018-10-04 11:11:21 +0100

    Added paging to backups page

--- src/main/java/com/github/cimsbioko/server/web/BackupsController.java
+++ src/main/java/com/github/cimsbioko/server/web/BackupsController.java
@@ -3,12 +3,14 @@ package com.github.cimsbioko.server.web;
 import com.github.cimsbioko.server.dao.BackupRepository;
 import com.github.cimsbioko.server.domain.Backup;
 import com.github.cimsbioko.server.service.BackupService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -25,18 +27,18 @@ public class BackupsController {
     }
 
     @GetMapping("/backups")
-    public String backups(ModelMap model) {
+    public String backups(ModelMap model, @RequestParam(name = "p", defaultValue = "0") Integer page) {
         if (!model.containsAttribute("backup")) {
             model.addAttribute("backup", new Backup());
         }
-        model.addAttribute("backups", repo.findAll());
+        model.addAttribute("backups", repo.findAll(new PageRequest(page, 10)));
         return "backups";
     }
 
     @PostMapping("/backups")
     public String createBackup(@Valid @ModelAttribute("backup") Backup backup, BindingResult bindingResult, ModelMap model, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
-            return backups(model);
+            return backups(model, 0);
         } else {
             service.createBackup(backup.getName(), backup.getDescription());
             redirectAttributes.addFlashAttribute("queued", Boolean.TRUE);
