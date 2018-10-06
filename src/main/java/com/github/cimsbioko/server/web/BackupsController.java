bc2b682ddc099ff2f19a9ed108059beb56dfc2da
Brent Atkinson <brent.atkinson@gmail.com>
2018-10-06 03:26:02 +0100

    Initial work for rewrite of backup creation using ajax

--- src/main/java/com/github/cimsbioko/server/web/BackupsController.java
+++ src/main/java/com/github/cimsbioko/server/web/BackupsController.java
@@ -3,46 +3,100 @@ package com.github.cimsbioko.server.web;
 import com.github.cimsbioko.server.dao.BackupRepository;
 import com.github.cimsbioko.server.domain.Backup;
 import com.github.cimsbioko.server.service.BackupService;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Controller
 public class BackupsController {
 
     private BackupService service;
     private BackupRepository repo;
+    private MessageSource messages;
 
-    BackupsController(BackupService service, BackupRepository repo) {
+    BackupsController(BackupService service, BackupRepository repo, MessageSource messages) {
         this.service = service;
         this.repo = repo;
+        this.messages = messages;
     }
 
     @GetMapping("/backups")
-    public String backups(ModelMap model, @RequestParam(name = "p", defaultValue = "0") Integer page) {
-        if (!model.containsAttribute("backup")) {
-            model.addAttribute("backup", new Backup());
-        }
-        model.addAttribute("backups", repo.findAll(new PageRequest(page, 10)));
-        return "backups";
+    public ModelAndView backups(@RequestParam(name = "p", defaultValue = "0") Integer page) {
+        return new ModelAndView("backups", "backups", repo.findAll(new PageRequest(page, 10)));
     }
 
     @PostMapping("/backups")
-    public String createBackup(@Valid @ModelAttribute("backup") Backup backup, BindingResult bindingResult, ModelMap model, RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            return backups(model, 0);
-        } else {
-            service.createBackup(backup.getName(), backup.getDescription());
-            redirectAttributes.addFlashAttribute("queued", Boolean.TRUE);
-            return "redirect:/backups";
+    @ResponseBody
+    public AjaxResult createBackup(@Valid @RequestBody Backup backup, Locale locale) {
+        service.createBackup(backup.getName(), backup.getDescription());
+        return new AjaxResult()
+                .addMessage(resolveMessage("backups.msg.queued", locale));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    public AjaxResult handleInvalidArgument(MethodArgumentNotValidException e, Locale locale) {
+        AjaxResult result = new AjaxResult();
+        BindingResult bindResult = e.getBindingResult();
+        result.addError(resolveMessage("input.msg.errors", locale));
+        bindResult.getGlobalErrors()
+                .stream()
+                .map(oe -> messages.getMessage(oe, locale))
+                .forEach(result::addError);
+        bindResult.getFieldErrors()
+                .forEach(fe -> result.addFieldError(fe.getField(), messages.getMessage(fe, locale)));
+        return result;
+    }
+
+    private String resolveMessage(String key, Locale locale, Object... args) {
+        return messages.getMessage(key, args, locale);
+    }
+
+    static class AjaxResult {
+
+        private List<String> messages = new ArrayList<>();
+        private List<String> errors = new ArrayList<>();
+        private Map<String, List<String>> fieldErrors = new HashMap<>();
+
+        AjaxResult addMessage(String message) {
+            messages.add(message);
+            return this;
+        }
+
+        AjaxResult addError(String error) {
+            errors.add(error);
+            return this;
+        }
+
+        AjaxResult addFieldError(String field, String error) {
+            fieldErrors.merge(field, singletonList(error),
+                    (a, b) -> Stream.concat(a.stream(), b.stream()).collect(toList()));
+            return this;
+        }
+
+        public List<String> getMessages() {
+            return messages;
+        }
+
+        public List<String> getErrors() {
+            return errors;
+        }
+
+        public Map<String, List<String>> getFieldErrors() {
+            return fieldErrors;
         }
     }
 }
