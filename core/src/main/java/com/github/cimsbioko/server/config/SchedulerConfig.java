f4571cd916759ecd29c4494e14467163dae4b8ab
Brent Atkinson <brent.atkinson@gmail.com>
2018-11-19 13:22:06 +0100

    Enabled websocket/stomp support server-side

--- core/src/main/java/com/github/cimsbioko/server/config/SchedulerConfig.java
+++ core/src/main/java/com/github/cimsbioko/server/config/SchedulerConfig.java
@@ -2,6 +2,7 @@ package com.github.cimsbioko.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @EnableScheduling
 public class SchedulerConfig {
     @Bean
+    @Primary
     public TaskScheduler taskScheduler() {
         return new ThreadPoolTaskScheduler();
     }
