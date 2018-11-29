69df825f96ec35cb3eb941552a29fe84ac88ec91
Brent Atkinson <brent.atkinson@gmail.com>
2018-11-29 13:56:21 +0100

    Removed mapping for '/' to fall back to index.html

--- core/src/main/java/com/github/cimsbioko/server/web/DefaultController.java
+++ core/src/main/java/com/github/cimsbioko/server/web/DefaultController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-
     @GetMapping("/login")
     public String login() {
         return "login";
