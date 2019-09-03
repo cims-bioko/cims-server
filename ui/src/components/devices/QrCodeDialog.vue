533064f117c128e911eb9d879c62ffca7e7f2133
Brent Atkinson <brent.atkinson@gmail.com>
2019-09-03 15:15:58 -0400

    CIMSS-84: Add a warning that qrcode is single-use only

--- ui/src/components/devices/QrCodeDialog.vue
+++ ui/src/components/devices/QrCodeDialog.vue
@@ -1,11 +1,12 @@
 <template>
     <b-modal ref="modal" :title="$t('qrcode.title', [name])" ok-only>
         <b-img center :src="codeUrl" />
+        <b-alert variant="warning" show>{{$t('qrcode.onetime')}}</b-alert>
     </b-modal>
 </template>
 
 <script>
-    import {BModal, BImg} from 'bootstrap-vue'
+    import {BModal, BImg, BAlert} from 'bootstrap-vue'
     export default {
         name: 'qr-code-dialog',
         props: {
@@ -29,7 +30,7 @@
             },
         },
         components: {
-            BModal, BImg
+            BModal, BImg, BAlert
         }
     }
 </script>
