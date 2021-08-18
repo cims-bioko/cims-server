2401f0d5ae7af6a75b3fdf3c765492d92022cd7b
Brent Atkinson <brent.atkinson@gmail.com>
2021-08-17 21:38:48 -0400

    Improve form field extractor

--- core/src/main/java/com/github/cimsbioko/server/service/FormFieldExtractor.kt
+++ core/src/main/java/com/github/cimsbioko/server/service/FormFieldExtractor.kt
@@ -1,5 +1,6 @@
 package com.github.cimsbioko.server.service
 
+import com.github.cimsbioko.server.service.Type.*
 import org.jdom2.Attribute
 import org.jdom2.Document
 import org.jdom2.Element
@@ -8,34 +9,41 @@ import org.jdom2.filter.Filters
 import org.jdom2.xpath.XPathExpression
 import org.jdom2.xpath.XPathFactory
 
+enum class Type {
+    STRING,
+    INT,
+    BOOLEAN,
+    DECIMAL,
+    DATE,
+    TIME,
+    DATE_TIME,
+    GEOPOINT,
+    GEOTRACE,
+    GEOSHAPE,
+    BINARY,
+    BARCODE,
+    INTENT,
+    REPEAT,
+    @Deprecated("not a type in recent odk xforms spec")
+    SELECT,
+    @Deprecated("not a type in recent odk xforms spec")
+    SELECT_ONE,
+    STRUCTURE,
+    UNKNOWN,
+}
+
 data class FormField(
     val name: String,
     val path: String,
     val order: Int = 0,
-    val type: Type = Type.UNKNOWN,
-    val binary: Boolean = false
-) {
-    enum class Type {
-        STRING,
-        INT,
-        BOOLEAN,
-        DECIMAL,
-        DATE,
-        TIME,
-        DATE_TIME,
-        GEOPOINT,
-        GEOTRACE,
-        GEOSHAPE,
-        BINARY,
-        BARCODE,
-        INTENT,
-        REPEAT,
-        STRUCTURE,
-        SELECT_ONE,
-        SELECT,
-        UNKNOWN,
-    }
-}
+    val type: Type = UNKNOWN,
+    val binary: Boolean = false,
+)
+
+data class Binding(
+    val nodeset: String,
+    val type: Type
+)
 
 interface FormFieldExtractor {
     fun extractFields(doc: Document): List<FormField>
@@ -47,7 +55,7 @@ private val XHTML_NS = Namespace.getNamespace("xhtml", "http://www.w3.org/1999/x
 
 class FormFieldExtractorImpl : FormFieldExtractor {
 
-    /* xpath expressions to extract important form metadata, not threadsafe so clone */
+    /* xpath expressions to extract form metadata, not threadsafe so clone */
     private var modelExpression: XPathExpression<Element>
     private var mainInstanceExpression: XPathExpression<Element>
     private var bindExpression: XPathExpression<Element>
@@ -56,7 +64,7 @@ class FormFieldExtractorImpl : FormFieldExtractor {
     init {
         val xpath = XPathFactory.instance()
         modelExpression = xpath.compile("/xhtml:html/xhtml:head/xforms:model", Filters.element(), null, XHTML_NS, XFORMS_NS)
-        mainInstanceExpression = xpath.compile("xforms:instance/.[1]", Filters.element(), null, XFORMS_NS)
+        mainInstanceExpression = xpath.compile("xforms:instance/*[1]", Filters.element(), null, XFORMS_NS)
         bindExpression = xpath.compile("xforms:bind[@type]", Filters.element(), null, XFORMS_NS)
         repeatExpression = xpath.compile(
             "/xhtml:html/xhtml:body//xforms:repeat[@nodeset]/@nodeset", Filters.attribute(), null, XHTML_NS, XFORMS_NS
@@ -79,7 +87,90 @@ class FormFieldExtractorImpl : FormFieldExtractor {
         return repeatExpression.clone().evaluate(doc)
     }
 
+    private fun String.absolute(root: String): String = if (startsWith("/")) this else "$root/$this"
+    private val Element.hasChildren
+        get() = children?.isNotEmpty() ?: false
+
+    private fun String.toType(): Type {
+        return when (this) {
+            "string" -> STRING
+            "int" -> INT
+            "boolean" -> BOOLEAN
+            "decimal" -> DECIMAL
+            "date" -> DATE
+            "time" -> TIME
+            "dateTime" -> DATE_TIME
+            "geopoint" -> GEOPOINT
+            "geotrace" -> GEOTRACE
+            "geoshape" -> GEOSHAPE
+            "binary" -> BINARY
+            "barcode" -> BARCODE
+            "intent" -> INTENT
+            "select1" -> SELECT_ONE
+            "select" -> SELECT
+            else -> UNKNOWN
+        }
+    }
+
+    private fun Element.toBinding(): Binding? {
+        val nodeset = getAttributeValue("nodeset")
+        val type = getAttributeValue("type")?.toType()
+        return if (nodeset != null && type != null) Binding(nodeset, type)
+        else null
+    }
+
     override fun extractFields(doc: Document): List<FormField> {
-        TODO("Not yet implemented")
+        val model = extractModel(doc)
+        val instance = model?.let { extractMainInstance(model) }
+        val bindings = model?.let { extractBinds(it) }?.mapNotNull { it.toBinding() }
+        val repeats = extractRepeats(doc).map { it.value }.toSet()
+        return if (instance == null || bindings == null) emptyList()
+        else formFields(instance, bindings, repeats)
+    }
+
+
+    private fun formFields(
+        instance: Element,
+        bindings: List<Binding>,
+        repeats: Set<String>,
+        envelope: String = instance.name,
+        basePath: List<String> = emptyList(),
+        orderStart: Int = 0
+    ): List<FormField> {
+        val fields: MutableList<FormField> = mutableListOf()
+        val processedRepeats: MutableSet<String> = mutableSetOf()
+        var order = orderStart
+        for (child in instance.children) {
+            val path = basePath + child.name
+            val joinedPath = path.joinToString(prefix = "/", separator = "/")
+            var field = FormField(
+                name = child.name,
+                path = joinedPath,
+                order = order
+            )
+            val bindingPath = "/${envelope}${joinedPath}"
+            if (bindingPath in repeats) {
+                if (bindingPath in processedRepeats) continue
+                else processedRepeats += bindingPath
+                field = field.copy(type = REPEAT)
+            } else {
+                val binding = bindings.find { it.nodeset.absolute(envelope) == bindingPath }
+                if (binding != null) field = field.copy(type = binding.type)
+                else if (child.hasChildren) field = field.copy(type = STRUCTURE)
+            }
+            fields += field
+            order += 1
+
+            if (field.type == BINARY || field.path == "/meta/audit") {
+                field = field.copy(binary = true)
+            }
+
+            if ((field.type == REPEAT || field.type == STRUCTURE) && child.hasChildren) {
+                val nested = formFields(child, bindings, repeats, envelope, path, order)
+                fields += nested
+                order += nested.size
+            }
+        }
+        return fields
     }
 }
--- core/src/test/java/com/github/cimsbioko/server/service/FormFieldExtractorTests.kt
+++ core/src/test/java/com/github/cimsbioko/server/service/FormFieldExtractorTests.kt
@@ -3,6 +3,7 @@ package com.github.cimsbioko.server.service
 import org.jdom2.Document
 import org.jdom2.input.SAXBuilder
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.BeforeClass
 import org.junit.Test
 
@@ -62,4 +63,58 @@ class FormFieldExtractorTest {
             }
         }
     }
+
+    @Test
+    fun testExtractFields() {
+        val fields = doc?.let { extractor?.extractFields(it) }
+        Assert.assertNotNull(fields)
+        fields?.also {
+            Assert.assertTrue(fields.isNotEmpty())
+            for ((idx, field) in MIS_DATA.withIndex()) {
+                assertEquals(field, fields[idx])
+            }
+        }
+    }
 }
+
+val MIS_DATA = listOf(
+    FormField("fieldWorkerExtId", "/fieldWorkerExtId", type= Type.STRING),
+    FormField("fieldWorkerUuid", "/fieldWorkerUuid", 1, type = Type.STRING),
+    FormField("collectionDateTime", "/collectionDateTime", 2, type = Type.STRING),
+    FormField("entityUuid", "/entityUuid", 3, type = Type.STRING),
+    FormField("entityExtId", "/entityExtId", 4, type = Type.STRING),
+    FormField("survey_date", "/survey_date", 5, type = Type.DATE),
+    FormField("Household", "/Household", 6, type = Type.STRING),
+    FormField("Nationality", "/Nationality", 7, type = Type.SELECT_ONE),
+    FormField("HousingType", "/HousingType", 8, type = Type.SELECT_ONE),
+    FormField("HousingTypeOther", "/HousingTypeOther", 9, type = Type.STRING),
+    FormField("Rooms", "/Rooms", 10, type = Type.INT),
+    FormField("SleepingRooms", "/SleepingRooms", 11, type = Type.INT),
+    FormField("Generalgoods", "/Generalgoods", 12, type = Type.STRUCTURE),
+    FormField("Radio", "/Generalgoods/Radio", 13, type = Type.SELECT_ONE),
+    FormField("Television", "/Generalgoods/Television", 14, type = Type.SELECT_ONE),
+    FormField("Computer", "/Generalgoods/Computer", 15, type = Type.SELECT_ONE),
+    FormField("Clock", "/Generalgoods/Clock", 16, type = Type.SELECT_ONE),
+    FormField("Sofa", "/Generalgoods/Sofa", 17, type = Type.SELECT_ONE),
+    FormField("Table", "/Generalgoods/Table", 18, type = Type.SELECT_ONE),
+    FormField("Amoire", "/Generalgoods/Amoire", 19, type = Type.SELECT_ONE),
+    FormField("Cabinet", "/Generalgoods/Cabinet", 20, type = Type.SELECT_ONE),
+    FormField("Fans", "/Generalgoods/Fans", 21, type = Type.SELECT_ONE),
+    FormField("AirCon", "/Generalgoods/AirCon", 22, type = Type.SELECT_ONE),
+    FormField("Refrigerator", "/Generalgoods/Refrigerator", 23, type = Type.SELECT_ONE),
+    FormField("Stove", "/Generalgoods/Stove", 24, type = Type.SELECT_ONE),
+    FormField("WashingMachine", "/Generalgoods/WashingMachine", 25, type = Type.SELECT_ONE),
+    FormField("Car", "/Generalgoods/Car", 26, type = Type.SELECT_ONE),
+    FormField("HouseHoldSprayed", "/HouseHoldSprayed", 27, type = Type.SELECT_ONE),
+    FormField("Sticker", "/Sticker", 28, type = Type.SELECT_ONE),
+    FormField("StickerDate", "/StickerDate", 29, type = Type.DATE),
+    FormField("HouseholdSize", "/HouseholdSize", 30, type = Type.INT),
+    FormField("HHVisitors", "/HHVisitors", 31, type = Type.SELECT_ONE),
+    FormField("HHVisitorCount", "/HHVisitorCount", 32, type = Type.INT),
+    FormField("Roster", "/Roster", 33, type = Type.STRING),
+    FormField("individual_count", "/individual_count", 34, type = Type.STRING),
+    FormField("individual", "/individual", 35, type = Type.REPEAT),
+    FormField("id", "/individual/id", 36, type = Type.STRING),
+    FormField("Name", "/individual/Name", 37, type = Type.STRING),
+    FormField("RelationToHead", "/individual/RelationToHead", 38, type = Type.SELECT_ONE),
+)
